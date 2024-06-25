9e13168368b6d35508b5fd42173c8883fd1ee239
mycodesgit <lowexian@gmail.com>
2024-06-25 13:50:57 +0800

    Update

--- resources/views/partials/control_set_sidebar.blade.php
+++ resources/views/partials/control_set_sidebar.blade.php
@@ -12,19 +12,19 @@
 <div class="ml-2 mr-2 mt-3 mb-3">
     <ul class="list-group">
         @auth('web')
-            @if(Auth::guard('web')->user()->isAdmin == '0')
+            @if(Auth::guard('web')->user()->role == '0')
                 <a href="{{ route('usersRead') }}" class="list-group-item {{ $usersActive }}">User's Account</a> 
                 <a href="{{ route('setconfigure') }}" class="list-group-item {{ $curconfActive }}">Setting Configure</a>
                 <a href="{{ route('setgradepassconfigure') }}" class="list-group-item {{ $gradeconfActive }}">Grades Password</a> 
                 <a href="{{ route('serverMaintenance') }}" class="list-group-item {{ $serverActive }}">Server</a>  
             @endif
-            @if(Auth::guard('web')->user()->isAdmin == '3')
+            @if(Auth::guard('web')->user()->role == '3')
                 <a href="{{ route('setgradepassconfigure') }}" class="list-group-item {{ $gradeconfActive }}">Grades Password</a>  
             @endif
         @endauth
 
         @auth('faculty')
-            @if(Auth::guard('faculty')->user()->isAdmin == '943')
+            @if(Auth::guard('faculty')->user()->role == '943')
             @endif
         @endauth
         
