7db53f7efd37b5fc2ab3f4e23a7413daad3655bd
mycodesgit <lowexian@gmail.com>
2024-07-01 11:10:26 +0800

    Update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -89,7 +89,7 @@
         </div>
         <footer class="main-footer text-sm text-center" style="background-color: #04401f;">
             <div class="float-right d-none d-sm-inline "></div>
-            <i class="text-light">CPSU - COAS V.2.0: Maintained and Managed by Management Information System Office (MISO) under the Leadership of Dr. Aladino C. Moraca Copyright © 2023 CPSU, All Rights Reserved</i>
+            <i class="text-light">CISS V.1.0: Maintained and Managed by Management Information System Office (MISO) under the Leadership of Dr. Aladino C. Moraca Copyright © 2023 CPSU, All Rights Reserved</i>
         </footer>
     </div>
     @include('portal.modal-terms')
