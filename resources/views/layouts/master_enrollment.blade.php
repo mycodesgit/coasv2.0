511b8c5ddd29a54407585b22cb2ce6f3c80b5d51
mycodesgit <lowexian@gmail.com>
2024-07-04 09:19:16 +0800

    Update

--- resources/views/layouts/master_enrollment.blade.php
+++ resources/views/layouts/master_enrollment.blade.php
@@ -414,7 +414,7 @@
 
             Swal.fire({
                 title: 'Are you sure?',
-                text: "You won't be able to revert this!",
+                text: "You won't be able to recover the Enrollment",
                 icon: 'warning',
                 showCancelButton: true,
                 confirmButtonColor: '#3085d6',
