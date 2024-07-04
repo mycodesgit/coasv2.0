03002240745131e062b356854a8a2e7579a48a00
mycodesgit <lowexian@gmail.com>
2024-07-04 17:12:57 +0800

    Update

--- resources/views/enrollment/studenroll/pdfrf/studRF.blade.php
+++ resources/views/enrollment/studenroll/pdfrf/studRF.blade.php
@@ -310,7 +310,7 @@
     				</td>
     			</tr>
     			<tr>
-    				<td colspan="4" style="padding-left: 5px">Balance for the @if($student->semester == 1) <span class="badge badge-primary">FIRST SEMESTER</span> @elseif($student->semester == 2) <span class="badge badge-success">2ND SEMESTER</span> @elseif($student->semester == 3) <span class="badge badge-secondary">SUMMER</span> @endif SY {{ $student->schlyear }} as of {{ \Carbon\Carbon::parse($student->upid)->format('d/m/Y'); }} is: {{ number_format($totalBalanceFee, 2) }}</td>
+    				<td colspan="4" style="padding-left: 5px">Balance for the @if($student->semester == 1) <span class="badge badge-primary">FIRST SEMESTER</span> @elseif($student->semester == 2) <span class="badge badge-success">2ND SEMESTER</span> @elseif($student->semester == 3) <span class="badge badge-secondary">SUMMER</span> @endif SY {{ $student->schlyear }} as of {{ \Carbon\Carbon::parse($student->updated_ats)->format('F d, Y'); }} is: {{ number_format($totalBalanceFee, 2) }}</td>
     			</tr>
     		</tbody>
     	</table>
