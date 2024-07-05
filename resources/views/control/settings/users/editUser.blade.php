55f3a86cc47d51fb468d1577d7e07771540639f9
mycodesgit <lowexian@gmail.com>
2024-07-05 13:30:19 +0800

    Update

--- resources/views/control/settings/users/editUser.blade.php
+++ resources/views/control/settings/users/editUser.blade.php
@@ -50,21 +50,21 @@ CISS V.1.0 || Edit User
                                         <div class="col-md-6">
                                             <label><span class="badge badge-secondary">User Level</span></label>
                                             <select class="form-control form-control-sm" name="role" required="" style="text-transform: uppercase;">
-                                                <option value="0" @if (old('type') == '0' || $user->role == '0') {{ 'selected' }} @endif>Administrator</option>
-                                                <option value="1" @if (old('type') == '1' || $user->role == '1') {{ 'selected' }} @endif>Guidance Officer</option>
-                                                <option value="2" @if (old('type') == '2' || $user->role == '2') {{ 'selected' }} @endif>Guidance Staff</option>
-                                                <option value="3" @if (old('type') == '3' || $user->role == '3') {{ 'selected' }} @endif>Registrar</option>
-                                                <option value="4" @if (old('type') == '4' || $user->role == '4') {{ 'selected' }} @endif>Registrar Staff</option>
-                                                <option value="5" @if (old('type') == '5' || $user->role == '5') {{ 'selected' }} @endif>College Dean</option>
-                                                <option value="6" @if (old('type') == '6' || $user->role == '6') {{ 'selected' }} @endif>Program Head</option>
-                                                <option value="7" @if (old('type') == '7' || $user->role == '7') {{ 'selected' }} @endif>College Staff</option>
-                                                <option value="8" @if (old('type') == '8' || $user->role == '8') {{ 'selected' }} @endif>Scholarship Head</option>
-                                                <option value="9" @if (old('type') == '9' || $user->role == '9') {{ 'selected' }} @endif>Scholarship Staff</option>
-                                                <option value="10" @if (old('type') == '10' || $user->role == '10') {{ 'selected' }} @endif>Assessment Head</option>
-                                                <option value="11" @if (old('type') == '11' || $user->role == '11') {{ 'selected' }} @endif>Assessment Staff</option>
-                                                <option value="12" @if (old('type') == '12' || $user->role == '12') {{ 'selected' }} @endif>MIS Staff</option>
-                                                <option value="13" @if (old('type') == '13' || $user->role == '13') {{ 'selected' }} @endif>MIS Director</option>
-                                                <option value="14" @if (old('type') == 14 || $user->role == '14') {{ 'selected' }} @endif>MIS Officer</option>
+                                                <option value="0" @if (old('role') == '0' || $user->role == '0') {{ 'selected' }} @endif>Administrator</option>
+                                                <option value="1" @if (old('role') == '1' || $user->role == '1') {{ 'selected' }} @endif>Guidance Officer</option>
+                                                <option value="2" @if (old('role') == '2' || $user->role == '2') {{ 'selected' }} @endif>Guidance Staff</option>
+                                                <option value="3" @if (old('role') == '3' || $user->role == '3') {{ 'selected' }} @endif>Registrar</option>
+                                                <option value="4" @if (old('role') == '4' || $user->role == '4') {{ 'selected' }} @endif>Registrar Staff</option>
+                                                <option value="5" @if (old('role') == '5' || $user->role == '5') {{ 'selected' }} @endif>College Dean</option>
+                                                <option value="6" @if (old('role') == '6' || $user->role == '6') {{ 'selected' }} @endif>Program Head</option>
+                                                <option value="7" @if (old('role') == '7' || $user->role == '7') {{ 'selected' }} @endif>College Staff</option>
+                                                <option value="8" @if (old('role') == '8' || $user->role == '8') {{ 'selected' }} @endif>Scholarship Head</option>
+                                                <option value="9" @if (old('role') == '9' || $user->role == '9') {{ 'selected' }} @endif>Scholarship Staff</option>
+                                                <option value="10" @if (old('role') == '10' || $user->role == '10') {{ 'selected' }} @endif>Assessment Head</option>
+                                                <option value="11" @if (old('role') == '11' || $user->role == '11') {{ 'selected' }} @endif>Assessment Staff</option>
+                                                <option value="12" @if (old('role') == '12' || $user->role == '12') {{ 'selected' }} @endif>MIS Staff</option>
+                                                <option value="13" @if (old('role') == '13' || $user->role == '13') {{ 'selected' }} @endif>MIS Director</option>
+                                                <option value="14" @if (old('role') == '14' || $user->role == '14') {{ 'selected' }} @endif>MIS Officer</option>
                                             </select>
                                         </div>
 
