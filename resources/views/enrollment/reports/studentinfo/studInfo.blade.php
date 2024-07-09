8c38f41b15937f47a44e30b4f0ddff67fc215847
mycodesgit <lowexian@gmail.com>
2024-07-09 11:05:24 +0800

    Update

--- resources/views/enrollment/reports/studentinfo/studInfo.blade.php
+++ resources/views/enrollment/reports/studentinfo/studInfo.blade.php
@@ -60,7 +60,7 @@ CISS V.1.0 || Student Info
                                             @elseif(Auth::user()->campus == 'CC') Cauayan 
                                         @endif
                                     </option>
-                                    @if (Auth::user()->isAdmin == 0)
+                                    @if (Auth::user()->role == 0)
                                         <option value="MC">Main</option>
                                         <option value="SCC">San Carlos</option>
                                         <option value="VC">Victorias</option>
