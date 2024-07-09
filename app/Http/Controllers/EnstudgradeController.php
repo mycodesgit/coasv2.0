1c57bb36cd04238b8e8ca7921e024de896382153
mycodesgit <lowexian@gmail.com>
2024-07-09 09:15:26 +0800

    Update

--- app/Http/Controllers/EnstudgradeController.php
+++ app/Http/Controllers/EnstudgradeController.php
@@ -116,7 +116,7 @@ class EnstudgradeController extends Controller
         //             ->get();
 
 
-        $grdpercentage = range(43, 78); 
+        $grdpercentage = range(44, 80); 
         $grdCode = GradeCode::whereIn('id', $grdpercentage)
                 ->orderByRaw('CASE WHEN id BETWEEN 44 AND 74 THEN id END DESC, id DESC')
                 ->get();
