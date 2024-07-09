c1889f39bd4b4d76ac2511e2e869a380bd2f4727
mycodesgit <lowexian@gmail.com>
2024-07-09 15:18:27 +0800

    Update

--- resources/views/enrollment/gradesheet/view_search.blade.php
+++ resources/views/enrollment/gradesheet/view_search.blade.php
@@ -188,7 +188,7 @@ CISS V.1.0 || Grading
                             @endif
                         </td>
                         <td>
-                            @if ($datagenstud->subjFgrade == 'INC' || $datagenstud->subjFgrade == 'Inc.')
+                            @if ($datagenstud->subjFgrade == 'INC' || $datagenstud->subjFgrade == 'Inc.' || $datagenstud->subjFgrade == 'inc')
                             @if ($datagenstud->compstat == 1 || empty($datagenstud->subjComp))
                                 @if (!empty($datagenstud->subjComp))
                                     <select class="form-control form-control-sm" name="subjComp" id="{{ $datagenstud->sgid }}" onchange="updateGradeComp(this.id, this.value)">
