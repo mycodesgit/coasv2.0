fdee6268c2be97e9301b72a7a8c708ab455783cd
mycodesgit <lowexian@gmail.com>
2024-07-15 09:13:24 +0800

    Update

--- public/js/ajax/examresult/exresultSerialize.js
+++ public/js/ajax/examresult/exresultSerialize.js
@@ -244,7 +244,7 @@ $(document).on('click', '.btn-updateresultexam', function() {
         $('#qualifier').prop('checked', false);
     }
 
-    if (uprawScore >= 90) {
+    if (uprawScore >= 80) {
         $('#secondQualifiersGroup').show();
     } else {
         $('#secondQualifiersGroup').hide();
