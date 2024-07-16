d4e6fdb14a04b4f14f4add7233fd4ecdac0a0b5a
mycodesgit <lowexian@gmail.com>
2024-07-16 13:26:46 +0800

    Update

--- app/Http/Controllers/EnStudentPerCurriculumController.php
+++ app/Http/Controllers/EnStudentPerCurriculumController.php
@@ -113,6 +113,7 @@ class EnStudentPerCurriculumController extends Controller
             ->where('program_en_history.studSec', $studSec)
             ->where('program_en_history.schlyear', $schlyear)
             ->where('program_en_history.semester', $semester)
+            ->where('students.campus', $campus)
             ->select('program_en_history.*', 'students.*', 'coasv2_db_schedule.programs.progAcronym', 'coasv2_db_schedule.programs.progName')
             ->orderBy('students.lname', 'ASC')
             ->get();
