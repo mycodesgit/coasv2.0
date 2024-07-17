29a9f4f84dcd9dfd0309fe5e64ede9dc1d1976c3
mycodesgit <lowexian@gmail.com>
2024-07-17 16:07:11 +0800

    Update

--- resources/views/enrollment/gradesheet/view_search.blade.php
+++ resources/views/enrollment/gradesheet/view_search.blade.php
@@ -237,7 +237,7 @@ CISS V.1.0 || Grading
                                         </a>
                                     </form>
                                     @endif
-                                    @if ($datagenstud->compstat == 2 && $datagenstud->compstat == ' ' )
+                                    @if ($datagenstud->compstat == 2)
                                     <form method="POST" action="{{ route('editCompletion', ['id' => $datagenstud->sgid]) }}" id="editCompletionForm">
                                         @csrf
                                         <a class="dropdown-item" id="editgradecompletionid" data-toggle="modal" data-target="#editCompletiongrades">
