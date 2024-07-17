673b41d49c65d3ccd04d3cb7bfef9dee0e845066
mycodesgit <lowexian@gmail.com>
2024-07-17 16:32:58 +0800

    Update

--- resources/views/enrollment/reports/studentcurr/studcoursepdf.blade.php
+++ resources/views/enrollment/reports/studentcurr/studcoursepdf.blade.php
@@ -24,7 +24,11 @@
 </head>
 <body>
     <div align="center" style="margin-top: -30px">
-        <img src="{{ public_path('template/img/studcourseheaderMain.png') }}" width="80%">
+        @if(Auth::guard('web')->user()->campus == 'MC')
+            <img src="{{ public_path('template/img/studcourseheaderMain.png') }}" width="80%">
+        @elseif(Auth::guard('web')->user()->campus == 'CA')
+            <img src="{{ public_path('template/img/studcourseheaderCandoni.jpg') }}" width="80%">
+        @endif
     </div>
     <div align="center" style="font-family: Calibri !important; color: #000 !important; margin-top: -35px;">
         <h4>
