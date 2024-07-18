9f99292bb8d0910b99b600fadf90af5373997e9e
mycodesgit <lowexian@gmail.com>
2024-07-18 12:34:39 +0800

    Update

--- app/Http/Controllers/EnStudReportCardController.php
+++ app/Http/Controllers/EnStudReportCardController.php
@@ -110,8 +110,8 @@ class EnStudReportCardController extends Controller
             $weightedSum = 0;
 
             foreach ($studrepcardsub as $subject) {
-                $creditEarned = $subject->creditEarned;
-                $subjFgrade = $subject->subjFgrade;
+                $creditEarned = (float)$subject->creditEarned;
+                $subjFgrade = (float)$subject->subjFgrade;
                 
                 $totalCredits += $creditEarned;
                 $weightedSum += $subjFgrade * $creditEarned;
