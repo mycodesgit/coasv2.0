286fa28d7a671bb36ae2176c1d92712fd677fea8
mycodesgit <lowexian@gmail.com>
2024-07-22 12:56:20 +0800

    Update

--- resources/views/assessment/assessreports/hebilling_listsearch.blade.php
+++ resources/views/assessment/assessreports/hebilling_listsearch.blade.php
@@ -148,10 +148,10 @@ CISS V.1.0 || Higher Education Billing
                                     <td>{{ $student['studUnit'] }}</td>
                                     <td></td>
                                     @foreach($student['fees'] as $feeType => $amount)
-            @if(strpos($feeType, 'TUITION -') === 0)
-                <td>{{ $amount }}</td>
-            @endif
-        @endforeach
+                                        @if(strpos($feeType, 'TUITION -') === 0)
+                                            <td>{{ $amount }}</td>
+                                        @endif
+                                    @endforeach
                                     <td></td>
                                     <td>{{ $student['fees']['ATHLETIC FEE'] ?? '' }}</td>
                                     <td>{{ $student['fees']['COMPUTER LAB FEE'] ?? '' }}</td>
