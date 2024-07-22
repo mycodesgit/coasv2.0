cb8464edbc63580144ef1b332c230d590c53f33d
mycodesgit <lowexian@gmail.com>
2024-07-22 15:26:01 +0800

    Update

--- public/js/ajax/examresult/acceptedSerialize.js
+++ public/js/ajax/examresult/acceptedSerialize.js
@@ -84,7 +84,7 @@ $(document).ready(function() {
                             '<div class="dropdown-menu">';
 
                         if (isCampus) {
-                            dropdown += '<a href="printPreEnrolment/srch/' + row.adid + '" class="dropdown-item btn-edit">' +
+                            dropdown += '<a href="acceptedList/printPreEnrolment/srch/' + row.adid + '" class="dropdown-item btn-edit">' +
                                 '<i class="fas fa-file-pdf"></i> Generate Pre-Enrollment' +
                                 '</a>';
 
