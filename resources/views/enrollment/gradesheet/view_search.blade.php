709fd4342f4d5290e0f84c9624880679a57ef4b3
mycodesgit <lowexian@gmail.com>
2024-07-23 13:15:35 +0800

    Update

--- resources/views/enrollment/gradesheet/view_search.blade.php
+++ resources/views/enrollment/gradesheet/view_search.blade.php
@@ -229,14 +229,14 @@ CISS V.1.0 || Grading
                                 </button>
                                 <div class="dropdown-menu" role="menu" style="">
                                      @if ($datagenstud->subjFgrade != 'FAILURE' && $datagenstud->subjFgrade != 'INC' && $datagenstud->subjFgrade != 'Inc.' && $datagenstud->subjFgrade != 'inc')
-        <form method="POST" action="{{ route('editGrade', ['id' => $datagenstud->sgid]) }}" id="editConfirmForm">
-            @csrf
-            <input type="hidden" name="subjID" value="{{ $datagenstud->sgid }}">
-            <a class="dropdown-item" id="editgradeid" data-toggle="modal" data-target="#editgrades">
-                <i class="fas fa-pen"></i> Edit Grades
-            </a>
-        </form>
-    @endif
+                                        <form method="POST" action="{{ route('editGrade', ['id' => $datagenstud->sgid]) }}" id="editConfirmForm">
+                                            @csrf
+                                            <input type="hidden" name="subjID" value="{{ $datagenstud->sgid }}">
+                                            <a class="dropdown-item" id="editgradeid" data-toggle="modal" data-target="#editgrades">
+                                                <i class="fas fa-pen"></i> Edit Grades
+                                            </a>
+                                        </form>
+                                    @endif
 
                                     @if ($datagenstud->compstat == 2 || empty($datagenstud->compstat))
                                         <form method="POST" action="{{ route('editCompletion', ['id' => $datagenstud->sgid]) }}" id="editCompletionForm">
