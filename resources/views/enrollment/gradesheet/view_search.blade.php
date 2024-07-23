e4f31e48398c2dd188ac350d5568debc331ff499
mycodesgit <lowexian@gmail.com>
2024-07-23 13:09:42 +0800

    Update

--- resources/views/enrollment/gradesheet/view_search.blade.php
+++ resources/views/enrollment/gradesheet/view_search.blade.php
@@ -229,21 +229,22 @@ CISS V.1.0 || Grading
                                 </button>
                                 <div class="dropdown-menu" role="menu" style="">
                                     @if ($datagenstud->subjFgrade != 'INC' && $datagenstud->subjFgrade != 'Inc.' && $datagenstud->subjFgrade != 'inc' && $datagenstud->subjFgrade == 'NN')
-                                    <form method="POST" action="{{ route('editGrade', ['id' => $datagenstud->sgid]) }}" id="editConfirmForm">
-                                        @csrf
-                                        <input type="hidden" name="subjID" value="{{ $datagenstud->sgid }}">
-                                        <a class="dropdown-item" id="editgradeid" data-toggle="modal" data-target="#editgrades">
-                                            <i class="fas fa-pen"></i> Edit Grades
-                                        </a>
-                                    </form>
+                                        <form method="POST" action="{{ route('editGrade', ['id' => $datagenstud->sgid]) }}" id="editConfirmForm">
+                                            @csrf
+                                            <input type="hidden" name="subjID" value="{{ $datagenstud->sgid }}">
+                                            <a class="dropdown-item" id="editgradeid" data-toggle="modal" data-target="#editgrades">
+                                                <i class="fas fa-pen"></i> Edit Grades
+                                            </a>
+                                        </form>
                                     @endif
+
                                     @if ($datagenstud->compstat == 2 || empty($datagenstud->compstat))
-                                    <form method="POST" action="{{ route('editCompletion', ['id' => $datagenstud->sgid]) }}" id="editCompletionForm">
-                                        @csrf
-                                        <a class="dropdown-item" id="editgradecompletionid" data-toggle="modal" data-target="#editCompletiongrades">
-                                            <i class="fa-solid fa-envelopes-bulk"></i> Completion
-                                        </a>
-                                    </form>
+                                        <form method="POST" action="{{ route('editCompletion', ['id' => $datagenstud->sgid]) }}" id="editCompletionForm">
+                                            @csrf
+                                            <a class="dropdown-item" id="editgradecompletionid" data-toggle="modal" data-target="#editCompletiongrades">
+                                                <i class="fa-solid fa-envelopes-bulk"></i> Completion
+                                            </a>
+                                        </form>
                                     @endif
                                 </div>
                             </div>
