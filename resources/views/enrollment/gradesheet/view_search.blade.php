4e4103697cd6f46a3b9184a03e8abc1df9ef46f1
mycodesgit <lowexian@gmail.com>
2024-07-23 13:27:20 +0800

    Update

--- resources/views/enrollment/gradesheet/view_search.blade.php
+++ resources/views/enrollment/gradesheet/view_search.blade.php
@@ -228,7 +228,7 @@ CISS V.1.0 || Grading
                                 <span class="sr-only">Toggle Dropdown</span>
                                 </button>
                                 <div class="dropdown-menu" role="menu" style="">
-                                    
+                                    @if ($datagenstud->subjFgrade != 'FAILURE' && $datagenstud->subjFgrade != 'INC' && $datagenstud->subjFgrade != 'Inc.' && $datagenstud->subjFgrade != 'inc')
                                         <form method="POST" action="{{ route('editGrade', ['id' => $datagenstud->sgid]) }}" id="editConfirmForm">
                                             @csrf
                                             <input type="hidden" name="subjID" value="{{ $datagenstud->sgid }}">
@@ -236,6 +236,7 @@ CISS V.1.0 || Grading
                                                 <i class="fas fa-pen"></i> Edit Grades
                                             </a>
                                         </form>
+                                    @endif
 
                                     @if ($datagenstud->compstat == 2 || empty($datagenstud->compstat))
                                     <form method="POST" action="{{ route('editCompletion', ['id' => $datagenstud->sgid]) }}" id="editCompletionForm">
