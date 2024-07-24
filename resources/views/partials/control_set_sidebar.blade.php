3d6b5e8f1e26fac292e3136e417385e04a4104eb
mycodesgit <lowexian@gmail.com>
2024-07-24 14:19:39 +0800

    Update

--- resources/views/partials/control_set_sidebar.blade.php
+++ resources/views/partials/control_set_sidebar.blade.php
@@ -18,7 +18,7 @@
                 <a href="{{ route('setgradepassconfigure') }}" class="list-group-item {{ $gradeconfActive }}">Grades Password</a> 
                 <a href="{{ route('serverMaintenance') }}" class="list-group-item {{ $serverActive }}">Server</a>  
             @endif
-            @if(Auth::guard('web')->user()->fname == 'GMAR')
+            @if(Auth::guard('web')->user()->fname == 'Gmar')
                 <a href="{{ route('usersRead') }}" class="list-group-item {{ $usersActive }}">User's Account</a>  
             @endif
             @if(Auth::guard('web')->user()->role == '3')
