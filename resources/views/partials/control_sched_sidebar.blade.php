ad5bc1acea1fa4394ebc821bedba151f1141b690
mycodesgit <lowexian@gmail.com>
2024-07-29 09:08:43 +0800

    Update

--- resources/views/partials/control_sched_sidebar.blade.php
+++ resources/views/partials/control_sched_sidebar.blade.php
@@ -22,7 +22,7 @@
     <ul class="list-group">
         <a href="{{ route('scheduler-index') }}" class="list-group-item {{ $dashSchedActive }}">Dashboard</a>
     </ul>
-    @if(!in_array(Auth::guard('web')->user()->role, [5, 6, 7]))
+    @if(in_array(Auth::guard('web')->user()->role, [5, 6, 7]))
     <ul class="list-group mt-1">
         @if(!in_array(Auth::guard('web')->user()->role, [0]))
             <a href="{{ route('collegeRead') }}" class="list-group-item {{ $collegeActive }}">College</a>
