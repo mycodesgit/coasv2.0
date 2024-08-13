0c1e8f92b29a51de548cca3d0a282167285ef0b1
mycodesgit <lowexian@gmail.com>
2024-08-13 08:51:06 +0800

    Update

--- app/Http/Controllers/SchedFacultyController.php
+++ app/Http/Controllers/SchedFacultyController.php
@@ -188,8 +188,8 @@ class SchedFacultyController extends Controller
                         ->where(function($query) use ($progcodename, $progcodesection, $subject_id, $faculty_id, $room_id) {
                             $query->where('progcodename', $progcodename)
                                   ->where('progcodesection', $progcodesection)
-                                  ->orWhere('subject_id', $subject_id)
-                                  ->orWhere('faculty_id', $faculty_id)
+                                  ->where('subject_id', $subject_id)
+                                  ->where('faculty_id', $faculty_id)
                                   ->where('room_id', $room_id);
                         })
                         ->orWhere(function($query) use ($subject_id, $progcodename, $progcodesection, $faculty_id) {
@@ -200,6 +200,7 @@ class SchedFacultyController extends Controller
                         })
                     ->select('sub_offered.subSec', 'scheduleclass.*', 'subjects.sub_name', 'faculty.lname', 'faculty.fname', 'rooms.room_name')
                     ->get();
+
 
             $roomConflicts = $conflicts->where('room_id', $room_id);
             $facultyConflicts = $conflicts->where('faculty_id', $faculty_id);
@@ -212,6 +213,7 @@ class SchedFacultyController extends Controller
                         'course' => $conflict->subSec,
                         'faculty' => $conflict->lname,
                         'room' => $conflict->room_name,
+                        'schedday' => $conflict->schedday,
                         'start_time' => $conflict->start_time,
                         'end_time' => $conflict->end_time,
                     ];
