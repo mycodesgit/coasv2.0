56b62c4429cc78ffeeb6d08fdcc2828122fde340
mycodesgit <lowexian@gmail.com>
2024-08-28 10:49:02 +0800

    Update

--- public/js/ajax/examresult/adbillingSerialize.js
+++ public/js/ajax/examresult/adbillingSerialize.js
@@ -40,7 +40,6 @@ $(document).ready(function() {
             { data: 'lname' },
             { data: 'fname' },
             { data: 'mname' },
-            { data: 'unisch_name' },
             { data: 'gender' },
             { data: 'bday' },
             { data: 'progAcronym' },
