a8fae46d8df27ead3c4e15e00043aa7a3850d7aa
mycodesgit <lowexian@gmail.com>
2024-08-28 16:39:19 +0800

    Update

--- resources/views/assessment/assessreports/reports/pdfpersemtemplate.blade.php
+++ resources/views/assessment/assessreports/reports/pdfpersemtemplate.blade.php
@@ -40,7 +40,7 @@
     </div>
 
     <div class="studinfolabel" style="margin-top: 25px">
-        <span style="font-weight: bold;">STUDENT ID NO.:</span> <span class="studinfoID"><strong> {{ $studfees->first()->stud_id }}</strong></span>
+        <span style="font-weight: bold;">STUDENT ID NO.:</span> <span class="studinfoID"><strong> {{ $studfees->first()->studID }}</strong></span>
         <span style="font-weight: bold; text-align: right !important; margin-left: 300px;">DATE:</span> <span><strong>{{ strtoupper(\Carbon\Carbon::now()->format('F j, Y')) }}</strong></span>
     </div> 
 
