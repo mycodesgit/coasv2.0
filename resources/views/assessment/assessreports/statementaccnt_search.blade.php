cf5cd2683be7665a08ac7c97792a611c0d8a9928
mycodesgit <lowexian@gmail.com>
2024-08-28 14:09:45 +0800

    Update

--- resources/views/assessment/assessreports/statementaccnt_search.blade.php
+++ resources/views/assessment/assessreports/statementaccnt_search.blade.php
@@ -98,19 +98,19 @@ CISS V.1.0 || Student Statements of Accounts Per Semester
                             <div class="form-row">
                                 <div class="col-md-3">
                                     <label><span class="badge badge-warning">Student ID Number</span></label>
-                                    <input type="text" class="form-control form-control-sm" value="{{ $studfees->first()->studID }}" readonly>
+                                    <input type="text" class="form-control form-control-sm text-bold" value="{{ $studfees->first()->studID }}" readonly>
                                 </div>
                                 <div class="col-md-3">
                                     <label><span class="badge badge-warning">Firstname</span></label>
-                                    <input type="text" class="form-control form-control-sm" value="{{ $studfees->first()->fname }}" readonly>
+                                    <input type="text" class="form-control form-control-sm text-bold" value="{{ $studfees->first()->fname }}" readonly>
                                 </div>
                                 <div class="col-md-3">
                                     <label><span class="badge badge-warning">Middle Initial</span></label>
-                                    <input type="text" class="form-control form-control-sm" value="{{ $studfees->first()->mname }}" readonly>
+                                    <input type="text" class="form-control form-control-sm text-bold" value="{{ $studfees->first()->mname }}" readonly>
                                 </div>
                                 <div class="col-md-3">
                                     <label><span class="badge badge-warning">Lastname</span></label>
-                                    <input type="text" class="form-control form-control-sm" value="{{ $studfees->first()->lname }}" readonly>
+                                    <input type="text" class="form-control form-control-sm text-bold" value="{{ $studfees->first()->lname }}" readonly>
                                 </div>
                             </div>
                         </div>
