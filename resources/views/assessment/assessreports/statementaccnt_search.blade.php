133e6deb63125323f3e20ec9aba59d43894a89e5
mycodesgit <lowexian@gmail.com>
2024-08-28 15:21:25 +0800

    Update

--- resources/views/assessment/assessreports/statementaccnt_search.blade.php
+++ resources/views/assessment/assessreports/statementaccnt_search.blade.php
@@ -114,7 +114,7 @@ CISS V.1.0 || Student Statements of Accounts Per Semester
                                 </div>
                                 <div class="col-md-2">
                                     <label>&nbsp;</label>
-                                    <a href="" class="btn btn-success form-control">
+                                    <a href="" class="btn btn-success form-control form-control-sm">
                                         <i class="fas fa-print"></i> Print
                                     </a>
                                 </div>
