c6b4151e78f3f4178430cfc80c9c7f1486af542f
mycodesgit <lowexian@gmail.com>
2024-08-28 14:21:15 +0800

    Update

--- resources/views/assessment/assessreports/statementaccnt_search.blade.php
+++ resources/views/assessment/assessreports/statementaccnt_search.blade.php
@@ -117,7 +117,7 @@ CISS V.1.0 || Student Statements of Accounts Per Semester
                     </div>
                 </div>
             </div>
-            <div class="col-md-7">
+            <div class="col-md-7 card card-warning card-outline">
                 <table id="" class="table table-hover table-striped table-bordered">
                     <thead>
                         <tr>
@@ -154,7 +154,7 @@ CISS V.1.0 || Student Statements of Accounts Per Semester
                     </tbody>
                 </table>
             </div>
-            <div class="col-md-5 card card-info card-outline">
+            <div class="col-md-5 card card-success card-outline">
                 <table id="" class="table table-hover table-striped table-bordered">
                     <thead>
                         <tr>
