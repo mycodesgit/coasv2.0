717c45ac4d4535af70478508641232f00ce5f826
mycodesgit <lowexian@gmail.com>
2024-08-28 14:28:49 +0800

    Update

--- resources/views/assessment/assessreports/statementaccnt_search.blade.php
+++ resources/views/assessment/assessreports/statementaccnt_search.blade.php
@@ -117,77 +117,81 @@ CISS V.1.0 || Student Statements of Accounts Per Semester
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 card card-success card-outline">
-                <table id="" class="table table-hover table-striped table-bordered">
-                    <thead>
-                        <tr>
-                            <center class="mt-3">
-                                <h4>Appraisal</h4>
-                            </center>
-                        </tr>
-                        <tr>
-                            <th>Code</th>
-                            <th>Fund</th>
-                            <th>Amount</th>
-                            <th>Year</th>
-                            <th>Semester</th>
-                            <th>Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @php
-                            $totalAmount = 0;
-                        @endphp
-                        @foreach($studfees as $datastudfeesview)
-                            @php
-                                $totalAmount += $datastudfeesview->amount;
-                            @endphp
+            <div class="col-md-6">
+                <div class="card card-success card-outline">
+                    <table id="" class="table table-hover table-striped table-bordered">
+                        <thead>
                             <tr>
-                                <td>{{ $datastudfeesview->fundID }}</td>
-                                <td>{{ $datastudfeesview->account }}</td>
-                                <td>{{ $datastudfeesview->amount  }}</td>
-                                <td>{{ $datastudfeesview->schlyear }}</td>
-                                <td>{{ $datastudfeesview->semester }}</td>
-                                <td>{{ Carbon\Carbon::parse($datastudfeesview->dateAssess)->format('M j, Y') }}</td>
+                                <center class="mt-3">
+                                    <h4>Appraisal</h4>
+                                </center>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                            <tr>
+                                <th>Code</th>
+                                <th>Fund</th>
+                                <th>Amount</th>
+                                <th>Year</th>
+                                <th>Semester</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php
+                                $totalAmount = 0;
+                            @endphp
+                            @foreach($studfees as $datastudfeesview)
+                                @php
+                                    $totalAmount += $datastudfeesview->amount;
+                                @endphp
+                                <tr>
+                                    <td>{{ $datastudfeesview->fundID }}</td>
+                                    <td>{{ $datastudfeesview->account }}</td>
+                                    <td>{{ $datastudfeesview->amount  }}</td>
+                                    <td>{{ $datastudfeesview->schlyear }}</td>
+                                    <td>{{ $datastudfeesview->semester }}</td>
+                                    <td>{{ Carbon\Carbon::parse($datastudfeesview->dateAssess)->format('M j, Y') }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <div class="col-md-6 card card-success card-outline">
-                <table id="" class="table table-hover table-striped table-bordered">
-                    <thead>
-                        <tr>
-                            <center class="mt-3">
-                                <h4>Payment</h4>
-                            </center>
-                        </tr>
-                        <tr>
-                            <th>OR</th>
-                            <th>Code</th>
-                            <th>Fund</th>
-                            <th>Amount</th>
-                            <th>Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @php
-                            $totalAmountPaid = 0;
-                        @endphp
-                        @foreach($studpayment as $datastudpaymentview)
-                            @php
-                                $totalAmountPaid += $datastudpaymentview->amountpaid;
-                            @endphp
+            <div class="col-md-6">
+                <div class="card card-success card-outline">
+                    <table id="" class="table table-hover table-striped table-bordered">
+                        <thead>
                             <tr>
-                                <td>{{ $datastudpaymentview->orno }}</td>
-                                <td>{{ $datastudpaymentview->fund }}</td>
-                                <td>{{ $datastudpaymentview->account }}</td>
-                                <td>{{ $datastudpaymentview->amountpaid  }}</td>
-                                <td>{{ Carbon\Carbon::parse($datastudpaymentview->datepaid)->format('M j, Y') }}</td>
+                                <center class="mt-3">
+                                    <h4>Payment</h4>
+                                </center>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                            <tr>
+                                <th>OR</th>
+                                <th>Code</th>
+                                <th>Fund</th>
+                                <th>Amount</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php
+                                $totalAmountPaid = 0;
+                            @endphp
+                            @foreach($studpayment as $datastudpaymentview)
+                                @php
+                                    $totalAmountPaid += $datastudpaymentview->amountpaid;
+                                @endphp
+                                <tr>
+                                    <td>{{ $datastudpaymentview->orno }}</td>
+                                    <td>{{ $datastudpaymentview->fund }}</td>
+                                    <td>{{ $datastudpaymentview->account }}</td>
+                                    <td>{{ $datastudpaymentview->amountpaid  }}</td>
+                                    <td>{{ Carbon\Carbon::parse($datastudpaymentview->datepaid)->format('M j, Y') }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
             @php
