83a99585db12055d34e7bce8f9a12e80e08bce1e
mycodesgit <lowexian@gmail.com>
2024-08-29 17:28:43 +0800

    Update

--- resources/views/enrollment/reports/enrollogs/listgradesearch_encode.blade.php
+++ resources/views/enrollment/reports/enrollogs/listgradesearch_encode.blade.php
@@ -104,7 +104,7 @@ CISS V.1.0 || Encode Grades Logs
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach($xsd as $logdata)
+                                @foreach($data as $logdata)
                                     <tr>
                                         <td>{{ $logdata->studsID }}</td>
                                         <td>{{ $logdata->studsID }}</td>
