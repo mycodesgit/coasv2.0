9ecb212a6f8964fd20ca159c40d3e40bf56aefd9
mycodesgit <lowexian@gmail.com>
2024-08-29 17:32:11 +0800

    Update

--- app/Http/Controllers/EnStudEncodeGradesLogController.php
+++ app/Http/Controllers/EnStudEncodeGradesLogController.php
@@ -75,7 +75,7 @@ class EnStudEncodeGradesLogController extends Controller
         $data = EncodedGrade::join('studgrades', 'studgrades_logs.grdeprimID', '=', 'studgrades.id')
                 ->join('coasv2_db_schedule.sub_offered', 'studgrades.subjID', '=', 'coasv2_db_schedule.sub_offered.id')
                 ->join('students', 'studgrades_logs.studsID', '=', 'students.stud_id')
-                ->leftJoin('coasv2_db_schedule.subjects', 'coasv2_db_schedule.sub_offered.subCode', '=', 'coasv2_db_schedule.subjects.sub_code')
+                ->join('coasv2_db_schedule.subjects', 'coasv2_db_schedule.sub_offered.subCode', '=', 'coasv2_db_schedule.subjects.sub_code')
                 ->select('coasv2_db_schedule.sub_offered.*', 'studgrades_logs.*', 'studgrades.*', 'studgrades.id as sgid', 'studgrades.status as gstat', 'students.*', 'coasv2_db_schedule.subjects.*')
                 ->where('coasv2_db_schedule.sub_offered.schlyear', $schlyear)
                 ->where('coasv2_db_schedule.sub_offered.semester', $semester)
