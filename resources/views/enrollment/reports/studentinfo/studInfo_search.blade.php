6fb1c0b81bb8c3e532b26889f26a0240729a0b35
mycodesgit <lowexian@gmail.com>
2024-09-03 19:16:12 +0800

    Update

--- resources/views/enrollment/reports/studentinfo/studInfo_search.blade.php
+++ resources/views/enrollment/reports/studentinfo/studInfo_search.blade.php
@@ -206,7 +206,7 @@ CISS V.1.0 || Student Info
                             </div>
                             <div class="col-md-2">
                                 <label><span class="badge badge-secondary">Update Birthday</span></label>
-                                <input type="date" name="bday" class="form-control form-control-sm" id="">
+                                <input type="date" name="bday" class="form-control form-control-sm" id="viewdatastudBday">
                             </div>
                             <div class="col-md-2">
                                 <label><span class="badge badge-warning">Birthday</span></label>
