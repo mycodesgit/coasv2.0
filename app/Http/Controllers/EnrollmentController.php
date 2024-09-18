89bb3cd0d99dfc4082051ebec9fabed1a5258632
mycodesgit <lowexian@gmail.com>
2024-09-18 14:00:29 +0800

    Update

--- app/Http/Controllers/EnrollmentController.php
+++ app/Http/Controllers/EnrollmentController.php
@@ -595,6 +595,12 @@ class EnrollmentController extends Controller
     {   
         if (in_array(Auth::guard('web')->user()->campus, ['MC', 'VC', 'HinC'])) {
             $sy = ConfigureCurrent::select('id', 'schlyear')
+                ->whereIn('id', function($query) {
+                    $query->select(DB::raw('MAX(id)'))
+                        ->from('settings_conf')
+                        ->groupBy('schlyear');
+                })
+                ->orderBy('id', 'DESC')
                 ->get();
         }
 
