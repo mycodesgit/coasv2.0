43bbaad05b9286127271c5fb84ee2ac7c6a575be
mycodesgit <lowexian@gmail.com>
2024-09-26 11:50:36 +0800

    Update

--- resources/views/cashier/officialreceipt/listsearch_oredit.blade.php
+++ resources/views/cashier/officialreceipt/listsearch_oredit.blade.php
@@ -179,7 +179,7 @@ CISS V.1.0 || Edit OR
                                 <h5 class="text-bold">Comments</h5>
                             </div>
 
-                            <input type="hidden" name="id" value="{{ $datacommentOR->studorcomentsprimID }}">
+                            <input type="hidden" name="id" value="{{ $datacommentOR->studorcomentsprimID ?? 'null' }}">
                             <input type="hidden" name="orno" value="{{ request('orno') }}">
                             <input type="hidden" name="studID" value="{{ request('stud_id') }}">
                             <input type="hidden" name="semester" value="{{ request('semester') }}">
