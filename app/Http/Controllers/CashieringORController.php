fecf7db0043dc72a1b08a0a892065f3ac8df3c39
mycodesgit <lowexian@gmail.com>
2024-10-01 13:36:16 +0800

    Update

--- app/Http/Controllers/CashieringORController.php
+++ app/Http/Controllers/CashieringORController.php
@@ -511,7 +511,7 @@ class CashieringORController extends Controller
                     'coasv2_db_admission.users.fname',
                     'coasv2_db_admission.users.lname',
                 )
-                ->orderBy('studpayment.orno', 'DESC')
+                ->orderBy('studpayment.created_at', 'DESC')
                 ->get();
 
 
