40692f8a0e4f4f2e7cbc58b879b98f2f4057a101
mycodesgit <lowexian@gmail.com>
2024-10-11 11:35:11 +0800

    Update

--- resources/views/partials/control_sched_sidebar.blade.php
+++ resources/views/partials/control_sched_sidebar.blade.php
@@ -28,13 +28,13 @@
             <a href="{{ route('programsRead') }}" class="list-group-item {{ $classProgActive }}">Programs</a>
             <a href="{{ route('roomsRead') }}" class="list-group-item {{ $roomActive }}">Rooms</a>
         @endif
-        @if(in_array(Auth::guard('web')->user()->role, [0, 12, 14, 15]))
+        @if(in_array(Auth::guard('web')->user()->role, [0, 3, 12, 14, 15]))
             <a href="{{ route('courseEnroll_list') }}" class="list-group-item {{ $classEnrollActive }}">Classes Enrolled</a>  
         @endif
         @if(in_array(Auth::guard('web')->user()->role, [0, 12, 14]))
             <a href="{{ route('faculty_design') }}" class="list-group-item {{ $facDesigActive }}">Designation</a>
         @endif
-        @if(in_array(Auth::guard('web')->user()->role, [0, 12, 14, 15]))
+        @if(in_array(Auth::guard('web')->user()->role, [0, 3, 12, 14, 15]))
             <a href="{{ route('subjectsOffered') }}" class="list-group-item {{ $suboffActive }}">Subject Offered</a>
         @endif
         @if(in_array(Auth::guard('web')->user()->role, [0, 5, 6, 7, 12, 14]))
