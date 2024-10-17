50ebc29cbeedcce9b5fd8d2beb3921623fa25af5
mycodesgit <lowexian@gmail.com>
2024-10-17 10:21:18 +0800

    Update

--- app/Http/Controllers/EnStudEnrolledController.php
+++ app/Http/Controllers/EnStudEnrolledController.php
@@ -82,7 +82,7 @@ class EnStudEnrolledController extends Controller
     
         $data = StudEnrolmentHistory::join('students', 'program_en_history.studentID', '=', 'students.stud_id')
                 ->join('coasv2_db_schedule.programs', 'program_en_history.progCod', '=', 'coasv2_db_schedule.programs.progCod')
-                ->select('students.lname', 'students.fname', 'students.mname', 'students.ext', 'students.region', 'program_en_history.progCod', 'program_en_history.studentID', 'program_en_history.studYear', 'program_en_history.studSec', 'program_en_history.schlyear', 'program_en_history.semester', 'coasv2_db_schedule.programs.progAcronym')
+                ->select('students.lname', 'students.fname', 'students.mname', 'students.ext', 'students.address', 'students.brgy', 'students.city', 'students.province', 'students.region', 'students.zcode', 'program_en_history.progCod', 'program_en_history.studentID', 'program_en_history.studYear', 'program_en_history.studSec', 'program_en_history.schlyear', 'program_en_history.semester', 'coasv2_db_schedule.programs.progAcronym')
                 ->where('program_en_history.campus', '=', $campus)
                 ->where('students.stud_id', 'NOT LIKE', '%-G')
                 ->where('program_en_history.schlyear', '=', $schlyear)
