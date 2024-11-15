0c113671b077a6ed7ca65c9f4c148f3892a26fad
mycodesgit <lowexian@gmail.com>
2024-11-15 10:48:52 +0800

    Update

--- app/Http/Controllers/EnStudReportCardController.php
+++ app/Http/Controllers/EnStudReportCardController.php
@@ -120,21 +120,8 @@ class EnStudReportCardController extends Controller
             }
         }
 
-        $latestProgram = DB::table('program_en_history')
-            ->select('studentID', DB::raw("SUBSTRING_INDEX(course, ' ', 1) as course"))
-            ->where('schlyear', $schlyear)
-            ->where('semester', $semester)
-            ->where('campus', $campus)
-            ->where('studentID', $stud_id)
-            ->whereYear('created_at', Carbon::now()->year)
-            ->orderBy('created_at', 'desc') // Make sure to use the correct timestamp column
-            ->limit(1);
-
         $studrepcard = StudEnrolmentHistory::join('students', 'program_en_history.studentID', '=', 'students.stud_id')
-                    ->joinSub($latestProgram, 'latest_history', function ($join) {
-                        $join->on('program_en_history.studentID', '=', 'latest_history.studentID');
-                    })
-                    ->join('coasv2_db_schedule.programs', DB::raw("SUBSTRING_INDEX(latest_history.course, ' ', 1)"), '=', 'coasv2_db_schedule.programs.progAcronym')
+                    ->leftJoin('coasv2_db_schedule.programs', 'program_en_history.progCod', '=', 'coasv2_db_schedule.programs.progCod')
                     ->join('studgrades', 'program_en_history.studentID', '=', 'studgrades.studID')
                     ->leftJoin('coasv2_db_schedule.sub_offered', 'studgrades.subjID', '=', 'coasv2_db_schedule.sub_offered.id')
                     ->leftJoin('coasv2_db_schedule.subjects', 'coasv2_db_schedule.sub_offered.subCode', '=', 'coasv2_db_schedule.subjects.sub_code')
