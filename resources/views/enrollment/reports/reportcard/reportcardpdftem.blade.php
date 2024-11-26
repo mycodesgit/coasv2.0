1b5eb308cacca1104f8cf17a0c9aea9c0102647a
mycodesgit <lowexian@gmail.com>
2024-11-26 13:47:04 +0800

    Update

--- resources/views/enrollment/reports/reportcard/reportcardpdftem.blade.php
+++ resources/views/enrollment/reports/reportcard/reportcardpdftem.blade.php
@@ -192,9 +192,9 @@
                 <thead>
                     <th colspan="2" style="font-weight: bold; font-size: 11pt; padding-left: 380px;">
                         @if(Auth::guard('web')->user()->campus == 'MC')
-                            ARLOS, ELYSA JANE S.<br><span style="padding-left: 1px;">OIC - University Registrar</span>
+                            ELYSA JANE S. ARLOS<br><span style="padding-left: 1px;">OIC - University Registrar</span>
                         @elseif(Auth::guard('web')->user()->campus == 'VC')
-                            PAJARILLO, JUNO E., DPA<br><span style="padding-left: 30px;">Campus Registrar</span>
+                            JUNO E. PAJARILLO, DPA<br><span style="padding-left: 30px;">Campus Registrar</span>
                         @endif
                     </th>
                 </thead>
