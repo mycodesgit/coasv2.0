e2a59a52f8e2e69cdbfdadaeecc59d52ce41eab2
mycodesgit <lowexian@gmail.com>
2025-01-02 09:40:37 +0800

    Update

--- app/Http/Controllers/EnProgStudEvalController.php
+++ app/Http/Controllers/EnProgStudEvalController.php
@@ -45,7 +45,7 @@ class EnProgStudEvalController extends Controller
 {
     public function loadstudsub()
     {
-        $sy = ConfigureCurrent::where('set_status', '=', '2')->get();
+        $sy = ConfigureCurrent::where('set_status', '=', '3')->get();
 
         return view('enrollment.evalstud.search_studeval', compact('sy'));
     }
