ba8074714863ec825256705b65f89dad35d78895
mycodesgit <lowexian@gmail.com>
2025-01-02 11:26:26 +0800

    Update

--- app/Http/Controllers/EnrollmentController.php
+++ app/Http/Controllers/EnrollmentController.php
@@ -765,7 +765,7 @@ class EnrollmentController extends Controller
 
     public function editsearchStud()
     {   
-        if (in_array(Auth::guard('web')->user()->campus, ['MC', 'VC', 'HinC', 'CC', 'SCC'])) {
+        if (in_array(Auth::guard('web')->user()->campus, ['MC', 'VC', 'HinC', 'CC', 'SCC', 'MP', 'SC'])) {
             // $sy = ConfigureCurrent::select('id', 'schlyear')
             //     ->whereIn('id', ['18'])
             //     ->orderBy('id', 'DESC')
