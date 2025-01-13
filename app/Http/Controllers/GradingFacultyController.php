cd9d8642a2b0e595a25fd851f3136019ac7682fe
mycodesgit <lowexian@gmail.com>
2025-01-13 22:03:28 +0800

    Update

--- app/Http/Controllers/GradingFacultyController.php
+++ app/Http/Controllers/GradingFacultyController.php
@@ -204,7 +204,7 @@ class GradingFacultyController extends Controller
                         ->count();
 
 
-        $cursttngs = ConfigureCurrent::whereIn('set_status', [2, 4])->first();
+        $cursttngs = ConfigureCurrent::where('set_status', [4])->first();
         $fac = Faculty::all();
 
         $desiredIds = [1, 74, 75, 76, 77];
