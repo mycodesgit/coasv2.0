989429ab7ee894383fdb6a6f197ff6cbe700f293
mycodesgit <lowexian@gmail.com>
2025-01-22 11:21:56 +0800

    Update

--- app/Http/Controllers/EnStudGrdeViewController.php
+++ app/Http/Controllers/EnStudGrdeViewController.php
@@ -32,12 +32,12 @@ class EnStudGrdeViewController extends Controller
         $stud_id = $request->query('stud_id');
         $campus = Auth::guard('web')->user()->campus;
 
-        $student = Student::where('campus', $campus)->where('stud_id', $stud_id)->get();
+        $student = Student::where('campus', $campus)->where('stud_id', $stud_id)->first();
         if (!$student) {
             return redirect()->back()->with('error', 'Student ID Number <strong>' . $stud_id . '</strong> does not exist.');
         }
 
-        $studauth = Student::where('stud_id', '=', $stud_id)->first();
+        $studauth = Student::where('stud_id', '=', $stud_id)->where('campus', $campus)->first();
 
         $studsubviewgrd = Grade::leftJoin('coasv2_db_schedule.sub_offered', 'studgrades.subjID', '=', 'coasv2_db_schedule.sub_offered.id')
                     ->leftJoin('coasv2_db_schedule.subjects', 'coasv2_db_schedule.sub_offered.subCode', '=', 'coasv2_db_schedule.subjects.sub_code')
