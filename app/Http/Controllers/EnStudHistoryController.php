605dad742bc893e26382e6aeee70fa4c96eb6777
mycodesgit <lowexian@gmail.com>
2025-01-22 11:24:56 +0800

    Update

--- app/Http/Controllers/EnStudHistoryController.php
+++ app/Http/Controllers/EnStudHistoryController.php
@@ -63,9 +63,11 @@ class EnStudHistoryController extends Controller
     public function fetchStudEnrollmentHistory(Request $request)
     {
         $stud_id = $request->input('stud_id');
-
+        $campus = Auth::guard('web')->user()->campus;
+        
         $enrollmentHistory = StudEnrolmentHistory::join('coasv2_db_schedule.programs', 'program_en_history.progCod', '=', 'coasv2_db_schedule.programs.progCod')
             ->where('studentID', $stud_id)
+            ->where('campus', $campus)
             ->select('program_en_history.*', 'coasv2_db_schedule.programs.progAcronym')
             ->orderBy('schlyear', 'ASC')
             ->get();
