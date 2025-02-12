b32f6a4c88148007664dbac6108283386d78c007
mycodesgit <lowexian@gmail.com>
2025-02-12 10:56:14 +0800

    Update

--- app/Http/Controllers/EnStudReportCardController.php
+++ app/Http/Controllers/EnStudReportCardController.php
@@ -208,7 +208,15 @@ class EnStudReportCardController extends Controller
         $stud_id = $request->stud_id;
         $campus = Auth::guard('web')->user()->campus;
 
-        $student = Student::whereRaw("FIND_IN_SET(?, campus)", [$campus])->where('stud_id', $stud_id)->first();
+        $campusArray = array_map('trim', explode(',', $campus));
+
+        $student = Student::where('stud_id', $stud_id)
+                    ->where(function ($q) use ($campusArray) {
+                        foreach ($campusArray as $campus) {
+                            $q->orWhere('campus', 'LIKE', "%$campus%");
+                        }
+                    })
+                    ->first();
         if (!$student) {
             return redirect()->back()->with('error', 'Student ID Number <strong>' . $stud_id . '</strong> does not exist.');
         }
