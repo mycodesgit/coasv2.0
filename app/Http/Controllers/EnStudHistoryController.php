a8aa182a92da2a4736449c17b4233e22e731522b
mycodesgit <lowexian@gmail.com>
2025-02-12 10:52:53 +0800

    Update

--- app/Http/Controllers/EnStudHistoryController.php
+++ app/Http/Controllers/EnStudHistoryController.php
@@ -87,7 +87,11 @@ class EnStudHistoryController extends Controller
 
         $enrollmentHistory = StudEnrolmentHistory::join('coasv2_db_schedule.programs', 'program_en_history.progCod', '=', 'coasv2_db_schedule.programs.progCod')
             ->where('program_en_history.studentID', $stud_id)
-            ->where('.program_en_historycampus', $campus)
+            ->where(function ($q) use ($campusArray) {
+                foreach ($campusArray as $campus) {
+                    $q->orWhere('program_en_history.campus', 'LIKE', "%$campus%");
+                }
+            })
             ->select('program_en_history.*', 'coasv2_db_schedule.programs.progAcronym')
             ->orderBy('schlyear', 'ASC')
             ->get();
