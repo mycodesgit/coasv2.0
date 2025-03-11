de5dd26138fdd0737dd57251e3f8b64650c3b514
mycodesgit <lowexian@gmail.com>
2025-03-11 15:48:39 +0800

    Update

--- app/Http/Controllers/StudHEBillingController.php
+++ app/Http/Controllers/StudHEBillingController.php
@@ -72,6 +72,7 @@ class StudHEBillingController extends Controller
                         ->leftJoin('coasv2_db_assessment.student_appraisal', 'program_en_history.studentID', '=', 'coasv2_db_assessment.student_appraisal.studID')
                         ->where('program_en_history.schlyear',  $schlyear)
                         ->where('program_en_history.semester',  $semester)
+                        ->where('program_en_history.status',  2)
                         // ->where('program_en_history.campus',  $campus)
                         ->where(function ($q) use ($campusArray) {
                             foreach ($campusArray as $campus) {
