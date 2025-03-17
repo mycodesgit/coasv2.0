dc088f8adfe9a5ae22a53d8deb02dcf93901d7a5
mycodesgit <lowexian@gmail.com>
2025-03-17 14:49:21 +0800

    Update

--- resources/views/portal/apply.blade.php
+++ resources/views/portal/apply.blade.php
@@ -406,7 +406,7 @@
                                                         <select class="form-control form-control-sm" name="campus" id="campus">
                                                             <option disabled selected>Select</option>
                                                             <option value="MC" @if (old('type') == 'MC') {{ 'selected' }} @endif disabled>Main (Closed)</option>
-                                                            <option value="VC" @if (old('type') == 'VC') {{ 'selected' }} @endif>Victorias</option>
+                                                            <option value="VC" @if (old('type') == 'VC') {{ 'selected' }} @endif disabled>Victorias (Closed)</option>
                                                             <option value="SCC" @if (old('type') == 'SCC') {{ 'selected' }} @endif disabled>San Carlos (Closed)</option>
                                                             <option value="MP" @if (old('type') == 'MP') {{ 'selected' }} @endif disabled>Moises Padilla (Closed)</option>
                                                             <option value="HC" @if (old('type') == 'HC') {{ 'selected' }} @endif disabled>Hinigaran (Closed)</option>
