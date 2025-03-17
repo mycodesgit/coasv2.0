40f4bc85977dd93d0ffb7de60882a65f0081e8c8
mycodesgit <lowexian@gmail.com>
2025-03-17 14:48:07 +0800

    Update

--- resources/views/portal/apply.blade.php
+++ resources/views/portal/apply.blade.php
@@ -413,7 +413,7 @@
                                                             <option value="IC" @if (old('type') == 'IC') {{ 'selected' }} @endif disabled>Ilog (Closed)</option>
                                                             <option value="CA" @if (old('type') == 'CA') {{ 'selected' }} @endif disabled>Candoni (Closed)</option>
                                                             <option value="CC" @if (old('type') == 'CC') {{ 'selected' }} @endif disabled>Cauayan (Closed)</option>
-                                                            <option value="SC" @if (old('type') == 'SC') {{ 'selected' }} @endif disabled>Sipalay (Closed)</option>
+                                                            <option value="SC" @if (old('type') == 'SC') {{ 'selected' }} @endif>Sipalay</option>
                                                             <option value="HinC" @if (old('type') == 'HinC') {{ 'selected' }} @endif disabled>Hinobaan (Closed)</option>
                                                             <option value="VE" @if (old('type') == 'VE') {{ 'selected' }} @endif>Valladolid</option>
                                                         </select>
