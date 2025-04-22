329971b28de207308a2023a859bbb67c8c934841
mycodesgit <lowexian@gmail.com>
2025-04-22 15:02:48 +0800

    Update

--- app/Http/Controllers/EnStudReportCardController.php
+++ app/Http/Controllers/EnStudReportCardController.php
@@ -39,6 +39,8 @@ use App\Models\AssessmentDB\StudentAppraisal;
 use App\Models\SettingDB\ConfigureCurrent;
 
 use Maatwebsite\Excel\Facades\Excel;
+use Maatwebsite\Excel\Concerns\FromView;
+
 use App\Exports\StudentEvalExport;
 
 class EnStudReportCardController extends Controller
