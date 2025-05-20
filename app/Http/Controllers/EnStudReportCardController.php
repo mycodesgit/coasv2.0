d79dc48d92abe20160c05209e7721317e423a4c2
mycodesgit <lowexian@gmail.com>
2025-05-20 11:41:38 +0800

    Update

--- app/Http/Controllers/EnStudReportCardController.php
+++ app/Http/Controllers/EnStudReportCardController.php
@@ -365,7 +365,9 @@ class EnStudReportCardController extends Controller
 
         // $firstYear = !empty($studrepcardsub) ? $studrepcardsub->first()->schlyear : '2024-2025';
         $firstYear = ($studrepcardsub && $studrepcardsub->isNotEmpty()) ? $studrepcardsub->first()->schlyear : '2024-2025';
-        $isOldSystem = (intval(substr($firstYear, 0, 4)) < 2022);
+        //$isOldSystem = (intval(substr($firstYear, 0, 4)) < 2022);
+        $entryYear = (int)substr($stud_id, 0, 4);
+        $isOldSystem = $entryYear <= 2021;
 
         foreach ($studrepcardsub as $subject) {
             $creditEarned = (float)$subject->creditEarned;
