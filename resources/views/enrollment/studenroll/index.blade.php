80a70b33913069c591ab3db793e48e14bb7b08eb
mycodesgit <lowexian@gmail.com>
2025-05-21 15:54:30 +0800

    Update

--- resources/views/enrollment/studenroll/index.blade.php
+++ resources/views/enrollment/studenroll/index.blade.php
@@ -37,7 +37,7 @@ CISS V.1.0 || Enroll Student
             </div> 
         </div>
             <div class="row">
-                @if(in_array(Auth::guard('web')->user()->role, [3,4,5,6,7,12,13,14,15]) && Auth::guard('web')->user()->campus !== 'MC')
+                @if(in_array(Auth::guard('web')->user()->role, [3,4,5,6,7,12,13,14,15]) && Auth::guard('web')->user()->campus !== 'CC')
                     <div class="col-md-12">
                         <div class="alert alert-secondary alert-dismissible mt-3">
                             <div class="form-group mt-3">
