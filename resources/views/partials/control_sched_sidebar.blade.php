101700089dc0a22d9a429094ff928512d7312796
mycodesgit <lowexian@gmail.com>
2025-05-26 12:00:02 +0800

    Update

--- resources/views/partials/control_sched_sidebar.blade.php
+++ resources/views/partials/control_sched_sidebar.blade.php
@@ -28,7 +28,7 @@
         @if(in_array(Auth::guard('web')->user()->role, [0]))
             <a href="{{ route('collegeRead') }}" class="list-group-item {{ $collegeActive }}">College</a>
         @endif
-        @if(in_array(Auth::guard('web')->user()->role, [0]))
+        @if(in_array(Auth::guard('web')->user()->role, [0, 12, 14]))
             <a href="{{ route('programsRead') }}" class="list-group-item {{ $classProgActive }}">Programs</a>
             <a href="{{ route('roomsRead') }}" class="list-group-item {{ $roomActive }}">Rooms</a>
         @endif
