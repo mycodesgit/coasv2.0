4dea60ac78f1c7a772a16b27a7345053b87b414a
mycodesgit <lowexian@gmail.com>
2025-06-03 11:43:29 +0800

    Update

--- app/Http/Controllers/AdAcceptedController.php
+++ app/Http/Controllers/AdAcceptedController.php
@@ -105,7 +105,8 @@ class AdAcceptedController extends Controller
                     'coasv2_db_enrollment.students.stud_id'
                 )
                 ->where('ad_applicant_admission.year', $year)
-                ->where('ad_applicant_admission.campus', $campus)
+                ->where('ad_applicant_admission.year', $year)
+                ->whereRaw('LEFT(coasv2_db_enrollment.students.created_at, 4) = ?', [$year])
                 ->whereIn('ad_applicant_admission.p_status', [5, 6]);
 
         if ($strand) {
