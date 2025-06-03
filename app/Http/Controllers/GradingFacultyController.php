b32406573ac9d4deb0849a07d8a9bb2465c92357
mycodesgit <lowexian@gmail.com>
2025-06-03 09:39:21 +0800

    Update

--- app/Http/Controllers/GradingFacultyController.php
+++ app/Http/Controllers/GradingFacultyController.php
@@ -110,7 +110,7 @@ class GradingFacultyController extends Controller
 
         $substudcount = $sub->count();
 
-        $grdpercentage = array_merge(range(44, 78), [81, 82, 83, 84]);
+        $grdpercentage = array_merge(range(44, 78), [81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91]);
 
         $grdCode = GradeCode::whereIn('id', $grdpercentage)
                 ->orderByRaw('CASE WHEN id BETWEEN 44 AND 74 THEN id END DESC, id DESC')
