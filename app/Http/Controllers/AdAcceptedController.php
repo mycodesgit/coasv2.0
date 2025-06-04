3f12ba25cfd372e9bbba8a8fc61315e2c649fe38
mycodesgit <lowexian@gmail.com>
2025-06-04 15:55:40 +0800

    Update

--- app/Http/Controllers/AdAcceptedController.php
+++ app/Http/Controllers/AdAcceptedController.php
@@ -71,6 +71,7 @@ class AdAcceptedController extends Controller
                 ->where('ad_applicant_admission.year', $year)
                 ->where('ad_applicant_admission.campus', $campus)
                 ->where('coasv2_db_enrollment.students.campus', $campus)
+                ->where('coasv2_db_enrollment.students.stud_id', 'like', now()->format('Y') . '%')
                 ->where('ad_applicant_dept_rating.deptcol', $user)
                 ->whereIn('ad_applicant_admission.p_status', [5, 6]);
 
