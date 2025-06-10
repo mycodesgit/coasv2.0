89830bee3829f0583ca6b4bf8369209675b2c6dc
mycodesgit <lowexian@gmail.com>
2025-06-10 16:58:53 +0800

    Update

--- resources/views/enrollment/studenroll/enrollStudent.blade.php
+++ resources/views/enrollment/studenroll/enrollStudent.blade.php
@@ -142,7 +142,9 @@ CISS V.1.0 || Enroll Student
                                                 <select class="form-control form-control-sm" name="studSch">
                                                     <option disabled {{ empty($selectedScholar) ? 'selected' : '' }}> --SELECT SCHOLARSHIP-- </option>
                                                     @foreach ($studscholar as $data)
-                                                        <option value="{{ $data->id }}">{{ $data->scholar_name }}</option>
+                                                        <option value="{{ $data->id }}" {{ $selectedScholar == $data->id ? 'selected' : '' }}>
+                                                            {{ $data->scholar_name }}
+                                                        </option>
                                                     @endforeach
                                                 </select>
                                             </div>
