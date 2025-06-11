0355591adf27bc9c80c91c3ff9000d5221602a40
mycodesgit <lowexian@gmail.com>
2025-06-11 13:10:54 +0800

    Update

--- resources/views/scheduler/schedule/pdf/schedulefaculty_pdf.blade.php
+++ resources/views/scheduler/schedule/pdf/schedulefaculty_pdf.blade.php
@@ -243,7 +243,7 @@
         <span style="font-size: 8pt; font-weight: normal;">Signature over printed name</span>
     </div> --}}
 
-    <div class="details-sm" style="margin-top: 30px;">
+    <div class="details-sm" style="margin-top: 15px;">
         <span style="display: inline-block; width: 80px; vertical-align: top;">Prepared by:</span>
         <span style="display: inline-block; width: 80px; vertical-align: top; margin-left: 150px;">Noted:</span>
         <span style="display: inline-block; width: 80px; vertical-align: top; margin-left: 190px;">Reveiwed by:</span>
