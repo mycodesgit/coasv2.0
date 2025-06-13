e45296c99ee898eba0a6019fcfce4a9f22085453
mycodesgit <lowexian@gmail.com>
2025-06-13 10:34:42 +0800

    Update

--- public/js/chart/enbarchartperyearlevscholarship.js
+++ public/js/chart/enbarchartperyearlevscholarship.js
@@ -14,7 +14,7 @@ $(function () {
     var barColors = undercolors.map(() => getRandomColor());
 
     var currSemesterGradData = {
-        labels: underprogramScholar,
+        //labels: underprogramScholar,
         datasets: [{
             label: 'No. of Students Enrolled in ' + currSemesterLabel + ' (' + schlyearActive + ')',
             data: currunderprogramenrolmentScholarCounts,
