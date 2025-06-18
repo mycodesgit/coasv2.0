e967dae8577b63e2d0783a7ee129c50e6831a187
mycodesgit <lowexian@gmail.com>
2025-06-18 09:26:14 +0800

    Update

--- app/Http/Controllers/EnProgStudEvalController.php
+++ app/Http/Controllers/EnProgStudEvalController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 use App\Rules\UniqueStudentID;
 use Illuminate\Support\Facades\Log;
 
