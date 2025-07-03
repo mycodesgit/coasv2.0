3c1421ceabcebbe1d7bf9f10a7823054380221fb
mycodesgit <lowexian@gmail.com>
2025-07-03 14:34:10 +0800

    Update

--- resources/views/layouts/master_faculty.blade.php
+++ resources/views/layouts/master_faculty.blade.php
@@ -64,6 +64,10 @@
             <button class="btn btn-default btn-sm" id="header-toggle" style="background-color: rgb(218, 218, 218);">
                 <i class="fas fa-bars"></i>
             </button>
+
+            <div class="d-block d-md-none" style="z-index: 999">
+                <img src="{{ asset('template/img/cpsulogov4.png') }}" style="width:80px;" class="center-top">
+            </div>
         </div>
     </header>
 
