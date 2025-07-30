2f663c6ea20527c4ba1209fb9c7e2e7211aa9616
mycodesgit <lowexian@gmail.com>
2025-07-30 13:22:28 +0800

    Update

--- app/Http/Controllers/EnStudentPerCurriculumController.php
+++ app/Http/Controllers/EnStudentPerCurriculumController.php
@@ -331,7 +331,7 @@ class EnStudentPerCurriculumController extends Controller
             ->where('program_en_history.campus', $campus)
             ->where(function ($q) use ($campusArray) {
                 foreach ($campusArray as $campus) {
-                    $q->orWhere('program_en_history.campus', 'LIKE', "$campus");
+                    $q->orWhere('students.campus', 'LIKE', "$campus");
                 }
             })
             ->select('program_en_history.*', 'students.*', 'coasv2_db_schedule.programs.progAcronym', 'coasv2_db_schedule.programs.progName')
