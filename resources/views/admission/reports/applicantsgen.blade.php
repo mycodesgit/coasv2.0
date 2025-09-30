cb2c154fc07a7cc085bcacf2ae2ee28196ec1739
mycodesgit <lowexian@gmail.com>
2025-09-30 13:55:33 +0800

    Update

--- resources/views/admission/reports/applicantsgen.blade.php
+++ resources/views/admission/reports/applicantsgen.blade.php
@@ -150,7 +150,7 @@ CISS V.1.0 || Applicant Reports
                     <tbody>
                         @php $no = 1; @endphp
                         @foreach($data as $applicant)
-                            @if ($applicant->p_status != 6)
+                            @if ($applicant->p_status != 7)   
                             <tr>
                                 <td>{{ $no++ }}</td>
                                 <td>{{ $applicant->admission_id }}</td>
--- resources/views/admission/reports/schedulesgen.blade.php
+++ resources/views/admission/reports/schedulesgen.blade.php
@@ -95,7 +95,7 @@ CISS V.1.0 || Admission Schedules
                     <tbody>
                         @php $no = 1; @endphp
                         @foreach($data as $applicant)
-                            @if ($applicant->p_status = [1,2])
+                            @if ($applicant->p_status != 7)  
                             <tr>
                                 <td>{{ $no++ }}</td>
                                 <td>{{ $applicant->admission_id }}</td>
