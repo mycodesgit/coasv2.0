e048d7403362a7ace9b7c292df1ff937812954ce
mycodesgit <lowexian@gmail.com>
2025-10-06 13:41:15 +0800

    Update

--- resources/views/admission/applicant/slot_search.blade.php
+++ resources/views/admission/applicant/slot_search.blade.php
@@ -106,14 +106,17 @@ use App\Models\AdmissionDB\AdmissionDate;
                         </tr>
                     </thead>
                     <tbody>
-                        
+                        @php
+                            use App\Models\AdmissionDB\Year;
+                            $currentYear = Year::where('status', 'On')->value('adyear');
+                        @endphp
                         @if ($slots =  Time::where('date', $date->date)->get())
                             @foreach($slots as $slot)
                                 <tr>
                                     <td>{{\Carbon\Carbon::createFromFormat('H:i:s',$slot->time)->format('h:i A')}}</td>
                                     <td>
                                         <span class="badge badge-secondary">
-                                            {{ $avail =  Applicant::where('time','=', $slot->time)->where('d_admission','=', $slot->date)->where('p_status','!=', 7)->where('campus','=', Auth::guard('web')->user()->campus)->count() }}
+                                            {{ $avail =  Applicant::where('time','=', $slot->time)->where('d_admission','=', $slot->date)->where('p_status','!=', 7)->whereYear('date', $currentYear)->where('campus','=', Auth::guard('web')->user()->campus)->count() }}
                                         </span> / {{ $slot->slots }}
                                     </td>
                                 </tr>
