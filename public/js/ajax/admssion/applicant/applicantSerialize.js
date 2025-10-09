46ad243b996267e646f098dca072e12f050cb6a1
mycodesgit <lowexian@gmail.com>
2025-10-09 14:09:24 +0800

    Update

--- public/js/ajax/admssion/applicant/applicantSerialize.js
+++ public/js/ajax/admssion/applicant/applicantSerialize.js
@@ -81,7 +81,7 @@ $(document).ready(function() {
                     } else if (data == 2) {
                         return '<span class="badge badge-warning">Reupload</span>';
                     } else if (data == 3) {
-                        return '<span class="badge badge-warning">Done Reupload</span>';
+                        return '<span class="badge badge-secondary">Done Reupload</span>';
                     } else {
                         return '<span class="badge badge-secondary">Unknown</span>';
                     }
