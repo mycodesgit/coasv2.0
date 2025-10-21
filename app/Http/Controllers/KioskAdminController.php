7081e8f58a8bfc96f32d4c9295c04c096c69ae05
mycodesgit <lowexian@gmail.com>
2025-10-21 14:14:54 +0800

    Update

--- app/Http/Controllers/KioskAdminController.php
+++ app/Http/Controllers/KioskAdminController.php
@@ -199,7 +199,7 @@ class KioskAdminController extends Controller
                 // ->where('program_en_history.campus', $campus)
                 ->where(function ($q) use ($campusArray) {
                     foreach ($campusArray as $campus) {
-                        $q->orWhere('program_en_history.campus', 'LIKE', "%$campus%");
+                        $q->orWhere('program_en_history.campus', 'LIKE', "$campus");
                     }
                 })
                 ->where('program_en_history.status', 2)
