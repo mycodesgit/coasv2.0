4fd064d01fda624721d761223643221cc91ba08a
mycodesgit <lowexian@gmail.com>
2025-10-29 22:23:47 +0800

    Update

--- resources/views/layouts/master_student.blade.php
+++ resources/views/layouts/master_student.blade.php
@@ -48,7 +48,7 @@
                         </li> -->
                     </ul>
                 </div>
-                <form class="d-flex">
+                <form class="">
                     <a href="{{ route('destory.logout') }}" class="btn btn-outline-secondary btn-sm">
                         <i class="fas fa-power-off"></i> Sign Out
                     </a>
