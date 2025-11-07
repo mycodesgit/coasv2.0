c7259777b3eca6349a1f1dd3c6cd239421e06a9a
mycodesgit <lowexian@gmail.com>
2025-11-07 10:58:11 +0800

    Update

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -103,23 +103,23 @@ class StudentController extends Controller
 
         $studauth = Student::where('stud_id', '=', $studentowner)->first();
 
-        $studfees = DB::table('student_appraisal')
-            ->leftJoin('studpayment', function($join) {
-                $join->on('student_appraisal.studID', '=', 'studpayment.studID')
-                    ->on('student_appraisal.account', '=', 'studpayment.account');
+        $studfees = DB::table('coasv2_db_schedule.student_appraisal')
+            ->leftJoin('coasv2_db_schedule.studpayment', function($join) {
+                $join->on('coasv2_db_schedule.student_appraisal.studID', '=', 'coasv2_db_schedule.studpayment.studID')
+                    ->on('coasv2_db_schedule.student_appraisal.account', '=', 'coasv2_db_schedule.studpayment.account');
             })
-            ->where('student_appraisal.studID', $studentowner)
+            ->where('coasv2_db_schedule.student_appraisal.studID', $studentowner)
             ->select(
-                'student_appraisal.schlyear',
-                'student_appraisal.semester',
-                'student_appraisal.fundID',
-                'student_appraisal.account',
-                DB::raw('SUM(student_appraisal.amount) as total_fee'),
+                'coasv2_db_schedule.student_appraisal.schlyear',
+                'coasv2_db_schedule.student_appraisal.semester',
+                'coasv2_db_schedule.student_appraisal.fundID',
+                'coasv2_db_schedule.student_appraisal.account',
+                DB::raw('SUM(coasv2_db_schedule.student_appraisal.amount) as total_fee'),
                 DB::raw('IFNULL(SUM(studpayment.amount), 0) as total_payment'),
-                DB::raw('(SUM(student_appraisal.amount) - IFNULL(SUM(studpayment.amount), 0)) as balance')
+                DB::raw('(SUM(coasv2_db_schedule.student_appraisal.amount) - IFNULL(SUM(coasv2_db_schedule.studpayment.amount), 0)) as balance')
             )
-            ->groupBy('student_appraisal.schlyear', 'student_appraisal.semester', 'student_appraisal.fundID', 'student_appraisal.account')
-            ->orderBy('student_appraisal.id', 'ASC')
+            ->groupBy('coasv2_db_schedule.student_appraisal.schlyear', 'coasv2_db_schedule.student_appraisal.semester', 'coasv2_db_schedule.student_appraisal.fundID', 'coasv2_db_schedule.student_appraisal.account')
+            ->orderBy('coasv2_db_schedule.student_appraisal.id', 'ASC')
             ->get();
 
         
