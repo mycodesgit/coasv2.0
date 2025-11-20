b9a372d95f5ae3a6c82255722cd98e5d2cd63cb7
mycodesgit <lowexian@gmail.com>
2025-11-20 14:16:31 +0800

    Update

--- resources/views/partials/control_en_sidebar.blade.php
+++ resources/views/partials/control_en_sidebar.blade.php
@@ -83,7 +83,7 @@
             <a href="{{ route('list_trans') }}" class="list-group-item {{ $studtransAllActive }}">Transfered Student</a>
         @endif
 
-        @if(Auth::guard('web')->user()->role == 0 || Auth::guard('web')->user()->fname == 'Rosalie')
+        @if(Auth::guard('web')->user()->role == 0 || Auth::guard('web')->user()->fname == 'Rosalie' || Auth::guard('web')->user()->lname == 'Ramada')
             <a href="{{ route('subjectsRead') }}" class="list-group-item {{ $subjectAllActive }}">Subjects</a>
         @endif
     </ul>
