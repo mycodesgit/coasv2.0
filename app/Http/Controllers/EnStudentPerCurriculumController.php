fa78d2c9026b535fea0222607ca2eaac352b7e31
mycodesgit <lowexian@gmai.com>
2025-12-01 09:57:02 +0800

    Update

--- app/Http/Controllers/EnStudentPerCurriculumController.php
+++ app/Http/Controllers/EnStudentPerCurriculumController.php
@@ -145,6 +145,7 @@ class EnStudentPerCurriculumController extends Controller
                         $q->orWhere('program_en_history.campus', 'LIKE', "$campus");
                     }
                 })
+                ->where('program_en_history.status', 2)
                 ->groupBy('program_en_history.progCod', 'program_en_history.studYear', 'program_en_history.studSec')
                 ->select(
                     'coasv2_db_schedule.programs.progCod', 
