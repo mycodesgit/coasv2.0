c95fb87a937f8d4e175142fc6a88e39e15dc7b13
mycodesgit <lowexian@gmai.com>
2025-12-03 14:59:17 +0800

    Update

--- resources/views/layouts/master_faculty.blade.php
+++ resources/views/layouts/master_faculty.blade.php
@@ -130,7 +130,6 @@
                             </div>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right">
-                            <div class="dropdown-title">Logged in 5 min ago</div>
                             <a href="#" class="dropdown-item has-icon">
                                 <i class="far fa-user"></i> Profile
                             </a>
