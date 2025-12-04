34ac4ee97f9c8019036ec57cf792a2fe3a0bf320
mycodesgit <lowexian@gmai.com>
2025-12-04 15:09:15 +0800

    Update

--- resources/views/enrollment/studenroll/pdfrf/studRF.blade.php
+++ resources/views/enrollment/studenroll/pdfrf/studRF.blade.php
@@ -374,10 +374,53 @@
     			</tr>
     		</thead>
     		<tbody>
+				@php
+					$progPrefix = explode('-', $student->progCod)[0];
+				@endphp
     			<tr>
-    				<td class="signatories-rf-sign"></td>
-    				<td class="signatories-rf-sign"></td>
-    				<td class="signatories-rf-sign"></td>
+    				<td class="signatories-rf-sign">
+						@if(Auth::guard('web')->user()->campus == 'MC')
+							@if($progPrefix == 'CAF')
+								<img src="{{ public_path('template/img/empsign/caf.png') }}" alt="signature" style="position: absolute; margin-top: -40px; right: 550px; width: 100px; z-index: 5;">
+							@elseif($progPrefix == 'CCS')
+								<img src="{{ public_path('template/img/empsign/ccs.png') }}" alt="signature" style="position: absolute; margin-top: -40px; right: 550px; width: 100px; z-index: 5;">
+							@elseif($progPrefix == 'CBM')
+								<img src="{{ public_path('template/img/empsign/cbm.png') }}" alt="signature" style="position: absolute; margin-top: -40px; right: 550px; width: 100px; z-index: 5;">
+							@elseif($progPrefix == 'CTE')
+								<img src="{{ public_path('template/img/empsign/cte.png') }}" alt="signature" style="position: absolute; margin-top: -40px; right: 550px; width: 100px; z-index: 5;">
+							@elseif($progPrefix == 'CJE')
+								<img src="{{ public_path('template/img/empsign/cje.png') }}" alt="signature" style="position: absolute; margin-top: -40px; right: 550px; width: 100px; z-index: 5;">
+							@elseif($progPrefix == 'COE')
+								<img src="{{ public_path('template/img/empsign/coe.png') }}" alt="signature" style="position: absolute; margin-top: -40px; right: 550px; width: 100px; z-index: 5;">
+							@elseif($progPrefix == 'CAS')
+								<img src="{{ public_path('template/img/empsign/cas.png') }}" alt="signature" style="position: absolute; margin-top: -40px; right: 550px; width: 100px; z-index: 5;">
+							@endif
+						@endif
+					</td>
+    				<td class="signatories-rf-sign">
+						@if(Auth::guard('web')->user()->campus == 'MC')
+							@if($progPrefix == 'CAF')
+								<img src="{{ public_path('template/img/empsign/caf.png') }}" alt="signature" style="position: absolute; margin-top: -40px; right: 300px; width: 100px; z-index: 5;">
+							@elseif($progPrefix == 'CCS')
+								<img src="{{ public_path('template/img/empsign/ccs.png') }}" alt="signature" style="position: absolute; margin-top: -40px; right: 300px; width: 100px; z-index: 5;">
+							@elseif($progPrefix == 'CBM')
+								<img src="{{ public_path('template/img/empsign/cbm.png') }}" alt="signature" style="position: absolute; margin-top: -40px; right: 300px; width: 100px; z-index: 5;">
+							@elseif($progPrefix == 'CTE')
+								<img src="{{ public_path('template/img/empsign/cte.png') }}" alt="signature" style="position: absolute; margin-top: -40px; right: 300px; width: 100px; z-index: 5;">
+							@elseif($progPrefix == 'CJE')
+								<img src="{{ public_path('template/img/empsign/cje.png') }}" alt="signature" style="position: absolute; margin-top: -40px; right: 300px; width: 100px; z-index: 5;">
+							@elseif($progPrefix == 'COE')
+								<img src="{{ public_path('template/img/empsign/coe.png') }}" alt="signature" style="position: absolute; margin-top: -40px; right: 300px; width: 100px; z-index: 5;">
+							@elseif($progPrefix == 'CAS')
+								<img src="{{ public_path('template/img/empsign/cas.png') }}" alt="signature" style="position: absolute; margin-top: -40px; right: 300px; width: 100px; z-index: 5;">
+							@endif
+						@endif
+					</td>
+    				<td class="signatories-rf-sign">
+						@if(Auth::guard('web')->user()->campus == 'MC')
+							<img src="{{ asset('template/img/empsign/assessment.png') }}" alt="signature" style="position: absolute; margin-top: -40px; right: 50px; width: 100px; z-index: 5;">
+						@endif
+					</td>
     			</tr>
     		</tbody>
     	</table>
