24f74f5a4032c9cf3a328f62fef1efaa6ff6277d
mycodesgit <lowexian@gmai.com>
2025-12-05 15:29:17 +0800

    Update

--- resources/views/partials/control_en_sidebar.blade.php
+++ resources/views/partials/control_en_sidebar.blade.php
@@ -79,7 +79,7 @@
             <a href="{{ route('studgradecorrection_search') }}" class="list-group-item {{ $gradeStudcorrectActive }}">Correction of Grades</a>
         @endif
 
-        @if(Auth::guard('web')->user()->role ==0 || Auth::guard('web')->user()->lname == 'Arlos' || Auth::guard('web')->user()->lname == 'Gallardo')
+        @if(Auth::guard('web')->user()->role ==0 || Auth::guard('web')->user()->lname == 'Arlos' || Auth::guard('web')->user()->lname == 'Gallardo' Auth::guard('web')->user()->fname == 'Regielyn')
             <a href="{{ route('list_trans') }}" class="list-group-item {{ $studtransAllActive }}">Transfered Student</a>
         @endif
 
