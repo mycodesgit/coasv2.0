7b04f7c55775f2728f2db5efb2ed9836e5cdd64d
mycodesgit <lowexian@gmail.com>
2026-02-27 15:15:05 +0800

    Update

--- resources/views/layouts/master_student.blade.php
+++ resources/views/layouts/master_student.blade.php
@@ -94,7 +94,7 @@
                 bottom: 10px;
                 left: 50%;
                 transform: translateX(-50%);
-                background: rgb(20, 82, 68, 0.9);
+                background: rgb(20, 82, 68, 0.95);
                 backdrop-filter: blur(1px);
                 width: 90%;
                 max-width: 400px;
