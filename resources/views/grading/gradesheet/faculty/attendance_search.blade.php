8d3be97b9f14dc30f839b0c8da9e2c40d662322b
mycodesgit <lowexian@gmail.com>
2026-03-01 21:43:42 +0800

    Update

--- resources/views/grading/gradesheet/faculty/attendance_search.blade.php
+++ resources/views/grading/gradesheet/faculty/attendance_search.blade.php
@@ -35,17 +35,17 @@ CISS V.1.0 || Attendance Sheet
                                 @endphp
 
                                 <div class="row">
+                                    @php
+                                        $randomColor = ($index < 10) ? $fixedColors[$index % count($fixedColors)] : $colors[array_rand($colors)];
+                                    @endphp
                                     @foreach($datafacsubprogen as $index => $attendfac)
-                                        @php
-                                            $randomColor = ($index < 10) ? $fixedColors[$index % count($fixedColors)] : $colors[array_rand($colors)];
-                                        @endphp
                                         <div class="col-6 col-sm-4 col-md-2 text-center mb-3">
                                             <h2>
                                                 <a href="{{ route('attendance_searchfacpdfpage', ['id' => $attendfac->subjID, 'schlyear'  => request('schlyear'), 'semester'  => request('semester')]) }}" class="text-dark">
-                                                <i class="fa-regular fa-file-lines folder-icon" aria-hidden="true" style="color: {{ $randomColor }}; font-size: 60px;"></i>
-                                                <br>
-                                                <span style="color: {{ $randomColor }}; font-size: 14px; display: inline-block; margin-top: 5px;"></span>
-                                                <span style="font-size: 12px; font-weight: bold;">{{ $attendfac->sub_name }} - {{ $attendfac->subSec }}</span>
+                                                    <i class="fa-regular fa-file-lines folder-icon" aria-hidden="true" style="color: {{ $randomColor }}; font-size: 60px;"></i>
+                                                    <br>
+                                                    <span style="color: {{ $randomColor }}; font-size: 14px; display: inline-block; margin-top: 5px;"></span>
+                                                    <span style="font-size: 12px; font-weight: bold;">{{ $attendfac->sub_name }} - {{ $attendfac->subSec }}</span>
                                                 </a>
                                             </h2>
                                         </div>
--- resources/views/grading/gradesheet/faculty/attendance_searchpdf.blade.php
+++ resources/views/grading/gradesheet/faculty/attendance_searchpdf.blade.php
@@ -15,57 +15,54 @@ CISS V.1.0 || Grading
 @yield('sidemenu')
 
 @section('workspace')
-    <section class="section mt-4">
-        <!-- <div class="section-header" style="border-radius: 20px !important;">
-            <h1>Blank Page</h1>
-        </div> -->
+    <div class="row">
+        <div class="col-12">
+            <div class="mb-6">
+                <h1 class="fs-5 mb-4 d-none d-md-block">Attendance Sheet <span style="font-size: 12pt; font-style: italic;" class="text-danger">(Click icon to view Attendance Sheet)</span></h1>
 
-        <div class="section-body">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card" style="border-radius: 20px">
-                        <div class="card-body">
-                            <h4>Attendance Sheet <span style="font-size: 12pt; font-style: italic;" class="text-danger">(Click icon to view Attendance Sheet)</span></h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="card" style="border-radius: 20px">
-                        <div class="card-body">
-                            @php
-                                $colors = ['red', 'green', 'lightblue', 'yellow', 'gray'];  // Colors for the folder icons
-                                $fixedColors = ['skyblue', 'green', 'orange', 'yellow', 'gray'];  // Set fixed colors for folders 1 to 10
-                            @endphp
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header pt-3">
+                                <h6 class="card-title">
+                                    <i class="ti ti-file"></i> View Attendance
+                                </h6>
+                            </div>
+                            <div class="card-body">
+                                @php
+                                    $colors = ['red', 'green', 'lightblue', 'yellow', 'gray'];  // Colors for the folder icons
+                                    $fixedColors = ['skyblue', 'green', 'orange', 'yellow', 'gray'];  // Set fixed colors for folders 1 to 10
+                                @endphp
 
-                            <div class="row mt-2" style="border-bottom: 1px solid #04401f;">
-                                @foreach($datafacsubprogen as $index => $attendfac)
+                                <div class="row">
                                     @php
                                         $randomColor = ($index < 10) ? $fixedColors[$index % count($fixedColors)] : $colors[array_rand($colors)];
                                     @endphp
-                                    <div class="col-6 col-sm-2 col-md-2 text-center mb-3">
-                                        <h2>
-                                            <a href="{{ route('attendance_searchfacpdfpage', ['id' => $attendfac->subjID, 'schlyear'  => request('schlyear'), 'semester'  => request('semester')]) }}" class="text-dark">
-                                                <i class="fa-regular fa-file-lines folder-icon" aria-hidden="true" style="color: {{ $randomColor }}; font-size: 60px;"></i>
-                                                <br>
-                                                <span style="color: {{ $randomColor }}; font-size: 14px; display: inline-block; margin-top: 5px;"></span>
-                                                <span style="font-size: 12px; font-weight: bold;">{{ $attendfac->sub_name }} - {{ $attendfac->subSec }}</span>
-                                            </a>
-                                        </h2>
-                                    </div>
-                                @endforeach
+                                    @foreach($datafacsubprogen as $index => $attendfac)
+                                        <div class="col-6 col-sm-4 col-md-2 text-center mb-3">
+                                            <h2>
+                                                <a href="{{ route('attendance_searchfacpdfpage', ['id' => $attendfac->subjID, 'schlyear'  => request('schlyear'), 'semester'  => request('semester')]) }}" class="text-dark">
+                                                    <i class="fa-regular fa-file-lines folder-icon" aria-hidden="true" style="color: {{ $randomColor }}; font-size: 60px;"></i>
+                                                    <br>
+                                                    <span style="color: {{ $randomColor }}; font-size: 14px; display: inline-block; margin-top: 5px;"></span>
+                                                    <span style="font-size: 12px; font-weight: bold;">{{ $attendfac->sub_name }} - {{ $attendfac->subSec }}</span>
+                                                </a>
+                                            </h2>
+                                        </div>
+                                    @endforeach
+                                </div>
+                                <div class="col-md-12">
+                                    @php
+                                        $id = request('id');
+                                    @endphp
+                                    <iframe src="{{ route('studsubjectsReadPDFfacattendance', ['id' => $id, 'schlyear' => request('schlyear'), 'semester' => request('semester')]) }}" style="width: 100%; height: 600px;" frameborder="0" class="mt-3"></iframe>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-12">
-                            @php
-                                $id = request('id');
-                            @endphp
-                            <iframe src="{{ route('studsubjectsReadPDFfacattendance', ['id' => $id, 'schlyear' => request('schlyear'), 'semester' => request('semester')]) }}" style="width: 100%; height: 600px;" frameborder="0" class="mt-3"></iframe>
-                        </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
 
 @endsection
