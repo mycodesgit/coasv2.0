fdb114099b4a72df1aaa2707c547785b7081ab21
mycodesgit <lowexian@gmail.com>
2026-03-01 22:25:44 +0800

    Update

--- resources/views/grading/gradesheet/faculty/facschedulenowSearchPDF.blade.php
+++ resources/views/grading/gradesheet/faculty/facschedulenowSearchPDF.blade.php
@@ -61,18 +61,20 @@ CISS V.1.0 || Grading
                                     </div>
                                 </form>
                                 <div class="col-md-12 mt-3">
-                                    <div class="card">
-                                        <div class="card-body">
-                                            <button id="viewSchedule" class="btn btn-success btn-xs ml-1">
-                                                <i class="fas fa-eye"></i> View Faculty Loading/Schedule
-                                            </button>
-                                            {{-- <button id="viewFacultyLoad" class="btn btn-secondary btn-xs ml-1">
-                                                <i class="fas fa-bars-progress"></i> Faculty Loading
-                                            </button> --}}
-                                            <button type="button" id="refreshSchedule" class="btn btn-danger btn-xs ml-1">
-                                                <i class="fas fa-sync"></i> Refresh
-                                            </button>  
-                                            <div id="schedule-grid" class="mt-4"></div>
+                                    <div class="table-responsive">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <button id="viewSchedule" class="btn btn-success btn-xs ml-1">
+                                                    <i class="fas fa-eye"></i> View Faculty Loading/Schedule
+                                                </button>
+                                                {{-- <button id="viewFacultyLoad" class="btn btn-secondary btn-xs ml-1">
+                                                    <i class="fas fa-bars-progress"></i> Faculty Loading
+                                                </button> --}}
+                                                <button type="button" id="refreshSchedule" class="btn btn-danger btn-xs ml-1">
+                                                    <i class="fas fa-sync"></i> Refresh
+                                                </button>  
+                                                <div id="schedule-grid" class="mt-4"></div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
