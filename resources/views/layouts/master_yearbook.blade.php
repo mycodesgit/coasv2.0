5992f2bd17d4d9e1cdcab0d3e98f3a8ba9837a60
mycodesgit <lowexian@gmail.com>
2026-03-12 08:15:25 +0800

    Update

--- resources/views/layouts/master_yearbook.blade.php
+++ resources/views/layouts/master_yearbook.blade.php
@@ -100,6 +100,19 @@
         .textbold{
             font-weight: bold;
         }
+        ::-webkit-scrollbar {
+            width: 3px !important;
+        }
+        ::-webkit-scrollbar-track {
+            background: #f1f1f1 !important;
+        }
+        ::-webkit-scrollbar-thumb {
+            background: #888 !important;
+            border-radius: 3px !important;
+        }
+        ::-webkit-scrollbar-thumb:hover {
+            background: #555 !important;
+        }
     </style>
 </head>
 
