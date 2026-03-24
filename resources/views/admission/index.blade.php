2045b3b0c7f8c8aeb096926e055d38c1963f7ce9
mycodesgit <lowexian@gmail.com>
2026-03-24 16:00:12 +0800

    Update

--- resources/views/admission/index.blade.php
+++ resources/views/admission/index.blade.php
@@ -4,8 +4,45 @@
 CISS V.1.0 || Admission
 @endsection
 
-@section('sideheader')
-<h4>Admission</h4>
-@endsection
-
 @yield('sidemenu')
+
+@section('workspace')
+    <div class="row">
+        <div class="col-12">
+            <div class="mb-6">
+                {{-- <h1 class="fs-5 mb-4 d-none d-md-block">Dashboard</h1> --}}
+                <div class="card" style=" background-color: #e9ecef; margin-top: -10px">
+                    <div class="card-body">
+                        <ol class="breadcrumb" style="margin-bottom: -3px;">
+                            <li class="breadcrumb-item">
+                                <a href="{{ route('home') }}" class="btn btn-success btn-sm text-light">
+                                    <i class="fas fa-home"></i>
+                                </a>
+                            </li>
+                            <li class="breadcrumb-item mt-1">Admission</li>
+                            <li class="breadcrumb-item active mt-1">Dashboard</li>
+                        </ol>
+                    </div>
+                </div>
+                <div class="row g-3 mb-3 mt-3">
+                    <div class="col-lg-12">
+                        <div class="card mb-3">
+                            <div class="card-body p-6">
+                                <div class="d-flex justify-content-between pb-2">
+                                    <div>
+                                        <h3 class="fw-bold h1">Sorry guys, We're working on this feature.</h3>
+                                        <span>Please check back soon!</span>
+                                    </div>
+                                    <div>
+                                        <i class="ti ti-server fs-1 text-success"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
