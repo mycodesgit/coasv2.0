fdc55c3109eae7c9b456a0a32a4ce94535e02382
mycodesgit <lowexian@gmail.com>
2026-04-16 10:51:03 +0800

    UPdate

--- app/Http/Controllers/EnstudgradeController.php
+++ app/Http/Controllers/EnstudgradeController.php
@@ -140,7 +140,12 @@ class EnstudgradeController extends Controller
                 ->leftJoin('coasv2_db_schedule.subjects as s', 'so2.subCode', '=', 's.sub_code')
                 ->where('so.schlyear', $schlyear)
                 ->where('so.semester', $semester)
-                ->where('so.campus', $campus)
+                // ->where('so.campus', $campus)
+                ->where(function ($q) use ($campusArray) {
+                    foreach ($campusArray as $campus) {
+                        $q->orWhere('so.campus', 'LIKE', "%$campus%");
+                    }
+                })
                 // ->where('studgrades.campus', $campus)
                 ->where(function ($q) use ($campusArray) {
                     foreach ($campusArray as $campus) {
