826ed3d407ec96ea808653709d5802ba39b399db
mycodesgit <lowexian@gmail.com>
2026-04-21 07:27:46 +0800

    Update

--- app/Http/Controllers/EnStudentPerSubjectController.php
+++ app/Http/Controllers/EnStudentPerSubjectController.php
@@ -107,10 +107,10 @@ class EnStudentPerSubjectController extends Controller
         $semester = $request->query('semester');   
         $campus = Auth::guard('web')->user()->campus;
 
-        // $studCountSubquery = DB::table('coasv2_db_enrollment.studgrades')
-        //     ->select('subjID', DB::raw('COUNT(subjID) as countstud'))
-        //     ->where('campus', $campus)
-        //     ->groupBy('subjID');
+        $studCountSubquery = DB::table('coasv2_db_enrollment.studgrades')
+            ->select('subjID', DB::raw('COUNT(subjID) as countstud'))
+            ->where('campus', $campus)
+            ->groupBy('subjID');
 
         $data = SubjectOffered::join('subjects', 'sub_offered.subCode', '=', 'subjects.sub_code')
             ->leftJoinSub($studCountSubquery, 'studgrades', function ($join) {
