eebf03df4693b8013c9fd1d0f9923ced09029f75
mycodesgit <lowexian@gmail.com>
2026-04-28 07:58:15 +0800

    Update

--- app/Http/Controllers/EnGradesheetLogbookController.php
+++ app/Http/Controllers/EnGradesheetLogbookController.php
@@ -129,7 +129,7 @@ class EnGradesheetLogbookController extends Controller
                         ->where('sub_offered.schlyear', $schlyear)
                         ->where('sub_offered.semester', $semester)
                         ->where('sub_offered.campus', $campus)
-                        //->where('faculty.dept', $collegeabbr)
+                        ->where('faculty.faccollege', $collegeabbr)
                         ->where('sub_offered.subCode', 'NOT LIKE', '%-GSS-%')
                         ->orderBy('faculty.lname', 'ASC')
                         ->groupBy('sub_offered.id')
