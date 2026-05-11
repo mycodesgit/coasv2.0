b2d6a0266d1bd9519662ae2c4a51c47467ff1a5c
mycodesgit <lowexian@gmail.com>
2026-05-11 09:42:03 +0800

    Update

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -48,7 +48,7 @@ class LoginController extends Controller
         $validatedUser = auth()->guard('web')->attempt([
             'email' => $request->email,
             'password' => $request->password,
-            'statuser' => 1,
+            // 'statuser' => 1,
         ]);
 
         if ($validatedUser) {
