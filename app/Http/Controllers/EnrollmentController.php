11a4f3b7d2b7461ff18aa1da2d33d9a456506676
mycodesgit <lowexian@gmail.com>
2026-05-12 10:16:12 +0800

    Update

--- app/Http/Controllers/EnrollmentController.php
+++ app/Http/Controllers/EnrollmentController.php
@@ -1178,11 +1178,12 @@ class EnrollmentController extends Controller
                     ->where('status', 4)
                     ->first();
 
-        $programEnHistory = StudEnrolmentHistory::where('program_en_history.studentID', $stud_id)
+        $programEnHistory = StudEnrolmentHistory::join('coasv2_db_admission.users', 'program_en_history.postedBy', '=', 'coasv2_db_admission.users.id')
+                ->where('program_en_history.studentID', $stud_id)
                 ->where('program_en_history.schlyear', $schlyear)
                 ->where('program_en_history.semester', '=', $semester)
                 ->where('program_en_history.campus', '=', $campus)
-                ->select('program_en_history.*')
+                ->select('program_en_history.*', 'coasv2_db_admission.users.lname', 'coasv2_db_admission.users.fname', 'coasv2_db_admission.users.id as uid')
                 ->first(); 
 
         if (!$programEnHistory) {
