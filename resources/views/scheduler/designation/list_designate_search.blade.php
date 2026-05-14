4b6a57fbe3f2b4495c752745a2aab480034fbb78
mycodesgit <lowexian@gmail.com>
2026-05-14 13:35:52 +0800

    Update

--- resources/views/scheduler/designation/list_designate_search.blade.php
+++ resources/views/scheduler/designation/list_designate_search.blade.php
@@ -143,6 +143,15 @@ CISS V.1.0 || Class Scheduler
                                                                     <option value="CampusAdmin">Campus Admin</option>
                                                                 </select> --}}
                                                                 <select name="designation" id="designation" class="form-control select2bs4 form-control-sm">
+                                                                    <optgroup label="Most Selected">
+                                                                        <option value="Dean">Dean</option>
+                                                                        <option value="Division Chair">Division Chair</option>
+                                                                        <option value="Program Head">Program Head</option>
+                                                                        <option value="Registrar">Registrar</option>
+                                                                        <option value="Assessment">Assessment</option>
+                                                                        <option value="CampusAdmin">Campus Administrator</option>
+                                                                    </optgroup>
+
                                                                     <optgroup label="A. University Wide">
                                                                         <option value="Vice President">Vice President</option>
                                                                         <option value="Dean">Dean</option>
