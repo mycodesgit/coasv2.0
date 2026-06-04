72b87147b95ad09ec8cf53bc56467619afbb2326
mycodesgit <lowexian@gmail.com>
2026-06-04 10:04:26 +0800

    Update

--- resources/views/scheduler/room/rlist.blade.php
+++ resources/views/scheduler/room/rlist.blade.php
@@ -62,7 +62,7 @@ CISS V.1.0 || Class Scheduler
         </div>
     </div>
 
-    <div class="modal fade" id="modal-room" tabindex="-1" aria-modal="true" role="dialog" aria-labelledby="modal-roomLabel" aria-hidden="true">
+    {{-- <div class="modal fade" id="modal-room" tabindex="-1" aria-modal="true" role="dialog" aria-labelledby="modal-roomLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
@@ -111,7 +111,7 @@ CISS V.1.0 || Class Scheduler
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 
     <div class="modal fade mt-6" id="editRoomModal" role="dialog" aria-labelledby="editRoomModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
