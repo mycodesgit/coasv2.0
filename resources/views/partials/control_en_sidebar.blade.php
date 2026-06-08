cd73f88b1bf0c745c919d234ca2505daf4566066
mycodesgit <lowexian@gmail.com>
2026-06-08 16:45:54 +0800

    Update

--- resources/views/partials/control_en_sidebar.blade.php
+++ resources/views/partials/control_en_sidebar.blade.php
@@ -110,7 +110,7 @@
         </li>
     @endif
     
-    @if(Auth::guard('web')->user()->role == '3' && Auth::guard('web')->user()->campus == 'MC' && Auth::guard('web')->user()->role == '0')
+    @if(in_array(Auth::guard('web')->user()->role, ['0', '3']) && Auth::guard('web')->user()->campus == 'MC')
         <li>
             <a class="nav-link" href="#">
                 <i class="ti ti-file-spreadsheet"></i><span class="nav-text">Correction of Grades</span>
