392b66f71d066607c75d40ebc83e272c99735960
mycodesgit <lowexian@gmail.com>
2026-06-08 08:28:38 +0800

    Update

--- resources/views/loginstudent.blade.php
+++ resources/views/loginstudent.blade.php
@@ -171,9 +171,9 @@
                                 <input type="checkbox" class="form-check-input" id="formCheck" onclick="myFunction()">
                                 <label for="formCheck" class="form-check-label text-secondary"><small>Show Password</small></label>
                             </div>
-                            <div class="forgot">
+                            {{-- <div class="forgot">
                                 <small><a href="{{ route('forgot.index') }}" class="">Forgot Password?</a></small>
-                            </div>
+                            </div> --}}
                         </div>
                         <div class="input-group mb-3">
                             <button class="btn btn-lg btn-success w-100 fs-6">Login</button>
