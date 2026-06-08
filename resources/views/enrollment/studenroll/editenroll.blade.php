eb72c3f234d3b869ac7b4cb38104c81755c46643
mycodesgit <lowexian@gmail.com>
2026-06-08 13:41:33 +0800

    Update

--- resources/views/enrollment/studenroll/editenroll.blade.php
+++ resources/views/enrollment/studenroll/editenroll.blade.php
@@ -70,7 +70,7 @@ CISS V.1.0 || Enrollment
                                             </div>
                                         </form>
                                         <div class="page-header" style="border-bottom: 1px solid #04401f;"></div>
-                                        @if(in_array(Auth::guard('web')->user()->campus, ['MC']))
+                                        @if(in_array(Auth::guard('web')->user()->campus, ['MC', 'VC', 'SCC', 'HC', 'MP', 'IC', 'CA', 'CC', 'SC', 'HinC']))
                                             @if($queueMode->statusqueue === 'Off')
 
                                             @else
