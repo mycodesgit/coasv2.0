da3916f8836d8042b11bc1ba5474b12542c53195
mycodesgit <lowexian@gmail.com>
2026-06-18 15:37:54 +0800

    Update

--- resources/views/partials/control_en_sidebar.blade.php
+++ resources/views/partials/control_en_sidebar.blade.php
@@ -149,7 +149,7 @@
 
     @if(in_array(Auth::guard('web')->user()->role, [0, 1, 2, 3, 4, 13, 14]) || Auth::guard('web')->user()->lname == 'Gargoles')
         <li>
-            <a class="nav-link {{ $studinfoActive }}" href="{{ Auth::guard('web')->user()->role == 0 ? route('student.index') : route('studInfo_search') }}">
+            <a class="nav-link {{ $studinfoActive }}" href="{{ Auth::guard('web')->user()->role == 0 ? route('student.index') : '' }}">
                 <i class="ti ti-user"></i><span class="nav-text">Student Information</span>
             </a>
         </li>
