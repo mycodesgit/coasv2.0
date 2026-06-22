f17ee641d47a23c8740ab224a207200270772809
mycodesgit <lowexian@gmail.com>
2026-06-22 17:30:09 +0800

    Update

--- resources/views/grading/gradesheet/faculty/services/viewfaceval/subslisteval.blade.php
+++ resources/views/grading/gradesheet/faculty/services/viewfaceval/subslisteval.blade.php
@@ -1434,7 +1434,7 @@ CISS V.1.0 || Faculty Services
                                                 @endif
                                             @endforeach
                                         @endif
-                                    @elseif($vicepres->id == 434)
+                                    @elseif(optional($vicepres)->id == 434)
                                         @if($setevalmode->statuseval === 'Off')
                                             <div class="col-12">
                                                 <div class="alert alert-warning d-flex align-items-center" role="alert">
