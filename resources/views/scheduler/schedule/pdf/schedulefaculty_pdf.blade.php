8e5106ca841645c390dc9b53f926932f637807de
mycodesgit <lowexian@gmail.com>
2026-06-30 09:07:01 +0800

    Update

--- resources/views/scheduler/schedule/pdf/schedulefaculty_pdf.blade.php
+++ resources/views/scheduler/schedule/pdf/schedulefaculty_pdf.blade.php
@@ -285,7 +285,7 @@
         <div style="display: inline-block; margin-left: 5px; vertical-align: top; text-align: center; border-bottom: 1px solid black; width: 195px; font-size: 9pt !important">
             <span style="font-weight: bold; font-size: 9pt !important; text-transform: uppercase;">
                 @if(Auth::guard('web')->user()->campus == 'MC')
-                    @if($lname != 'Jalandoni')
+                    @if(Auth::guard('web')->user()->lname != 'Jalandoni')
                         {{ $dean }}
                     @else
                         &nbsp;
