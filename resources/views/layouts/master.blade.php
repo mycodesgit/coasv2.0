434746a5a676c56b4515dda89e0f2284b1968f07
mycodesgit <lowexian@gmail.com>
2026-09-01 10:35:21 +0800

    Update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,7 +11,7 @@
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('uilibs/images/cpsulogov4.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('uilibs/images/cpsulogov4.png') }}">
 
-    <link rel="stylesheet" href="{{ asset('uilibs/css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('uilibs/css/main.css') }}?v={{ time() }}">
     <link rel="stylesheet" href="{{ asset('uilibs/css/custom.css') }}?v={{ time() }}">
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="{{ asset('uilibs/plugins/fontawesome-free-V6/css/all.min.css') }}">
